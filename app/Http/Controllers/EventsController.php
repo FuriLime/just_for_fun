f23a9af906ac9cf18db19ed2870d193fc4403c6b
furilime <sergey.ch.ysbm@gmail.com>
2016-02-19 11:56:23 +0200

    eventcontroller

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -35,13 +35,12 @@ class EventsController extends Controller {
                         if(Sentinel::getUser()->timezone){
                             $my_time_zone = Sentinel::getUser()->timezone;
                         } else {
-//                          $ip = $_SERVER["REMOTE_ADDR"];
-////                            $ip = '178.136.229.229';
-//                            $query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip));
-//                            if ($query && $query['status'] == 'success') {
-//                                $my_time_zone = $query['timezone'];
-                            $my_time_zone = 'Asia/Tokyo';
-//                            }
+                          $ip = $_SERVER["REMOTE_ADDR"];
+//                            $ip = '178.136.229.229';
+                            $query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip));
+                            if ($query && $query['status'] == 'success') {
+                                $my_time_zone = $query['timezone'];
+                            }
                         }
 					$date->setTimezone(new \DateTimeZone($my_time_zone));
 					$event_start_zero = $date;
