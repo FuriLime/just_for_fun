30d07e506b3d6c2db64b510dfc423644f363affb
furilime <sergey.ch.ysbm@gmail.com>
2016-02-20 13:34:33 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -477,12 +477,8 @@ class EventsController extends Controller {
                    $my_time_zone = Sentinel::getUser()->timezone;
                }
                else {
-                   $ip = $_SERVER["REMOTE_ADDR"];
-//               $ip = '178.136.229.229';
-                   $query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip));
-                   if ($query && $query['status'] == 'success') {
-                       $my_time_zone = $query['timezone'];
-                   }
+
+                   $my_time_zone = $query = 'Asia/Tokyo';
                }
            }
             else {
