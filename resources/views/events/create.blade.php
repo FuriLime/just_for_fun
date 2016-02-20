d1917cf095982ec86c600e301dca453dd00bc72f
furilime <sergey.ch.ysbm@gmail.com>
2016-02-20 18:45:26 +0200

    event page

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -250,7 +250,7 @@ Create New event
 
         var timezone = jstz.determine();
         $('#usertimezone').val(timezone);
-            console.log($('#usertimezone').val(timezone));
+            console.log($('#usertimezone'));
  });
     </script>
 	<script type='text/javascript' src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
