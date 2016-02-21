8bf5a8f7df45f8c4cf611e93cb7a517d3df57d42
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 21:23:19 +0200

    cxhx

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -267,8 +267,8 @@ Create New event
 //            console.log(usertimezone);
 //        });
         $.ajax({
-            url:'event/add',
-            type: 'GET',
+            //url:'event/add',
+            //type: 'GET',
             data: {
                 value: $("input[name=usertimezone]").val()
             },
