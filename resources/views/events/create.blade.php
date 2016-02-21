2c470ee22627d46ff111760776da00dfeda417cf
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 21:18:04 +0200

    event page

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -267,7 +267,7 @@ Create New event
 //            console.log(usertimezone);
 //        });
         $.ajax({
-            url:'/event/add',
+            url:'event/add',
             type: 'GET',
             data: {
                 value: $("input[name=usertimezone]").val()
