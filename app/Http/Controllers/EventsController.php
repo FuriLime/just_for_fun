a3c61378412ac7c234a36eae3d7bc5107078568e
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 16:51:16 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -116,6 +116,7 @@ class EventsController extends Controller {
             if ($request->isMethod('get')){
                 var_dump($request->is('ajax'));
             }
+            var_dump($request->is('ajax'));
 //            $name = $_GET['name'];
             $newLat = $request->input('data');
 //            var_dump($name);
