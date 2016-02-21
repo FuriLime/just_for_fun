2e299097ca260ff8da0c012786caa770cdcd8219
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 16:01:07 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -114,7 +114,7 @@ class EventsController extends Controller {
             $my_time_zone = 'Asia/Tokyo';
             $name = Input::all();
             if ($request->isMethod('get')){
-                dd($request->input('username', value('sadasd')));
+                dd($request->input('username', value('')));
             }
 //            $name = $_GET['name'];
             $newLat = $request->input('data');
