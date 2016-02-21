cf6766105bd609b0ccad219da63c638417908d0a
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 08:39:16 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -111,9 +111,21 @@ class EventsController extends Controller {
 			$finish_date = date('Y/m/d 20:00', $start_date_tmp);
 			$default_timezone = date_default_timezone_get();
             $timezone_select = self::getTimeZoneSelect();
-            $ip=$_SERVER["REMOTE_ADDR"];
-			$location = GeoIP::getLocation($ip);
-            $my_time_zone = $location['timezone'];
+//            $ip=$_SERVER["REMOTE_ADDR"];
+//			$location = GeoIP::getLocation($ip);
+            $ip     = $_SERVER['REMOTE_ADDR']; // means we got user's IP address
+            $json   = file_get_contents( 'http://smart-ip.net/geoip-json/' . $ip); // this one service we gonna use to obtain timezone by IP
+// maybe it's good to add some checks (if/else you've got an answer and if json could be decoded, etc.)
+            $ipData = json_decode( $json, true);
+
+            if ($ipData['timezone']) {
+                $my_time_zone = new DateTimeZone( $ipData['timezone']);
+                $now = new DateTime( 'now', $my_time_zone); // DateTime object corellated to user's timezone
+            } else {
+                // we can't determine a timezone - do something else...
+            }
+
+//            $my_time_zone = $location['timezone'];
 			return view('events.create', array(
 				'timezone_select' => $timezone_select,
 				'start_date' => $start_date,
