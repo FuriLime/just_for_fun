07f5e7dbacf6a3db993612e91fa7ddb7b2aa80c0
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 11:24:56 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -113,8 +113,7 @@ class EventsController extends Controller {
             $timezone_select = self::getTimeZoneSelect();
             $my_time_zone = 'Asia/Tokyo';
             $name = $request->all();
-            dd($name);
-//            dd($request->input('usertimezone'));
+            $my_time_zone = $name;
 			return view('events.create', array(
 				'timezone_select' => $timezone_select,
 				'start_date' => $start_date,
