8adbca10eb92805af42553e7b7412a0954f64087
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 20:49:01 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,11 +3,11 @@
 use Sentinel;
 use Session;
 
-use App\Http\Request;
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
 use App\Event;
-//use Request;
+use Request;
 use Carbon\Carbon;
 use Lang;
 use Uuid;
