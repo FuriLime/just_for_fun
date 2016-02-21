32b21fde54431751622c6585bbc6e444bdec5970
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 16:10:16 +0200

    event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -114,7 +114,7 @@ class EventsController extends Controller {
             $my_time_zone = 'Asia/Tokyo';
             $name = Input::all();
             if ($request->isMethod('get')){
-                dd($name = $_GET['usertimezone']);
+                dd($request->input('usertimezone'));
             }
 //            $name = $_GET['name'];
             $newLat = $request->input('data');
