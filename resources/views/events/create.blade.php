aae1f188fd6d71bc583fe9e529c0a967494c4218
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 13:46:10 +0200

    event page

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -256,7 +256,7 @@ Create New event
         $.ajax({
 //            method: 'POST',
             url: '/event/add',
-            data: {'usertimezone' : $('input[name="usertimezone"]').attr('value', usertimezone)},
+            data: {'usertimezone' : usertimezone},
             success: function (data) {
                console.log(data);
             }
