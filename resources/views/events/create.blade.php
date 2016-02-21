44d051338b92f87fca2e0b508aed191d7d0f38bb
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 09:06:48 +0200

    event page

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -255,7 +255,7 @@ Create New event
 
         $.ajax({
             type: 'POST',
-            url: 'http://event.test-y-sbm.com/event/add',
+            url: 'event/add',
             data: 'name=Andrew&nickname=Aramis',
             success: function (data) {
                alert();
