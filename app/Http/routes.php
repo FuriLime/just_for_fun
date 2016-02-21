55351a8d66078af7b9248fd37de368e6ee2ba46d
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 11:30:44 +0200

    event page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -163,7 +163,7 @@ Route::get('member', array('as' => 'member_home', 'uses' => 'AccountController@i
 
 # Events
 Route::resource('events', 'EventsController');
-Route::get('/event/add', 'EventsController@create');
+Route::get('/event/add{my_time_zone}', 'EventsController@create');
 Route::post('/event/add', 'EventsController@store');
 Route::get('events/{uuid}/delete', array('as' => 'events.delete', 'uses' => 'EventsController@getDelete'));
 Route::get('events/{uuid}/confirm-delete', array('as' => 'events.confirm-delete', 'uses' => 'EventsController@getModalDelete'));
