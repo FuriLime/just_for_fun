85dfe486be4d8080c23ee6ff7d3bae73cec5fbb3
furilime <sergey.ch.ysbm@gmail.com>
2016-02-21 21:48:42 +0200

    cxhx

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -163,11 +163,11 @@ Route::get('member', array('as' => 'member_home', 'uses' => 'AccountController@i
 
 # Events
 Route::resource('events', 'EventsController');
-if (Sentinel::check()) {
-    if (Sentinel::inRole('admin') || Sentinel::inRole('user')) {
+if (!Sentinel::check()) {
+
         Route::get('event/add', 'EventsController@create');
         Route::post('event/add', 'EventsController@store');
-    }
+
 }
 Route::get('events/{uuid}/delete', array('as' => 'events.delete', 'uses' => 'EventsController@getDelete'));
 Route::get('events/{uuid}/confirm-delete', array('as' => 'events.confirm-delete', 'uses' => 'EventsController@getModalDelete'));
