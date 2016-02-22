563d4282c53f23639417e4077bdfe2b415cd3ee1
furilime <sergey.ch.ysbm@gmail.com>
2016-02-22 14:01:18 +0200

    cxhx

--- resources/views/admin/events/show.blade.php
+++ resources/views/admin/events/show.blade.php
@@ -25,23 +25,22 @@ event
         <div class="panel panel-primary ">
             <div class="panel-heading clearfix">
                 <h4 class="panel-title"> <i class="livicon" data-name="list-ul" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
-                    {{--event {{ $event->id }}'s details--}}
-                    {{ var_dump($event) }}
+                    event {{ $event->id }}'s details
                 </h4>
             </div>
             <br />
             <div class="panel-body">
                 <table class="table">
 
-                    {{--<tr><td>id</td><td>{{ $event->id }}</td></tr>--}}
-                     {{--<tr><td>title</td><td>{{ $event['title'] }}</td></tr>--}}
-					{{--<tr><td>type</td><td>{{ $event['type'] }}</td></tr>--}}
-					{{--<tr><td>description</td><td>{{ $event['description'] }}</td></tr>--}}
-					{{--<tr><td>location</td><td>{{ $event['location'] }}</td></tr>--}}
-					{{--<tr><td>url</td><td>{{ $event['url'] }}</td></tr>--}}
-					{{--<tr><td>timezone</td><td>{{ $event['timezone'] }}</td></tr>--}}
-					{{--<tr><td>date</td><td>{{ $event['start'] }}</td></tr>--}}
-					{{--<tr><td>time</td><td>{{ $event['finish'] }}</td></tr>--}}
+                    <tr><td>id</td><td>{{ $event->id }}</td></tr>
+                     <tr><td>title</td><td>{{ $event['title'] }}</td></tr>
+					<tr><td>type</td><td>{{ $event['type'] }}</td></tr>
+					<tr><td>description</td><td>{{ $event['description'] }}</td></tr>
+					<tr><td>location</td><td>{{ $event['location'] }}</td></tr>
+					<tr><td>url</td><td>{{ $event['url'] }}</td></tr>
+					<tr><td>timezone</td><td>{{ $event['timezone'] }}</td></tr>
+					<tr><td>date</td><td>{{ $event['start'] }}</td></tr>
+					<tr><td>time</td><td>{{ $event['finish'] }}</td></tr>
 
 					
                 </table>
