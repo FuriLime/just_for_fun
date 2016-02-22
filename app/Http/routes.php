eafb5d6f64d4f30317fb085671f5ab77bde19891
furilime <sergey.ch.ysbm@gmail.com>
2016-02-22 09:28:50 +0200

    cxhx

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -163,12 +163,9 @@ Route::get('member', array('as' => 'member_home', 'uses' => 'AccountController@i
 
 # Events
 Route::resource('events', 'EventsController');
-if (!Sentinel::check()) {
-
         Route::get('event/add', 'EventsController@create');
         Route::post('event/add', 'EventsController@store');
 
-}
 Route::get('events/{uuid}/delete', array('as' => 'events.delete', 'uses' => 'EventsController@getDelete'));
 Route::get('events/{uuid}/confirm-delete', array('as' => 'events.confirm-delete', 'uses' => 'EventsController@getModalDelete'));
 Route::post('event/addtocalendar', array('as' => 'event.addtocalendar', 'uses' => 'EventsController@addToCalendar'));
