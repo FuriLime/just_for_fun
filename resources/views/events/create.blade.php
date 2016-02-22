5cf8f099e2f8021a0fdeb5633e2eac5af2a5561b
furilime <sergey.ch.ysbm@gmail.com>
2016-02-22 10:00:30 +0200

    cxhx

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -82,7 +82,7 @@ Create New event
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-calendar"></span>
                                 </span>
-                                <input class="form-control" size="16" id="start" name="start" type="datetime" value="{{$start_date}}">
+                                <input class="form-control" size="16" id="start" name="start" type="datetime"  min="{{$start_date}}" value="{{$start_date}}">
 
                             </div>
                         </div>
