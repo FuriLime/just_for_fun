71675644b391eac20f931d34207b5cc913387d15
furilime <sergey.ch.ysbm@gmail.com>
2016-02-23 11:27:16 +0200

    cxhx

--- resources/views/emails/register-activate.blade.php
+++ resources/views/emails/register-activate.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <p>Hello {!! $user->first_name !!},</p>
 
-<p>Welcome to SiteNameHere! Please click on the following link to confirm your SiteNameHere account:</p>
+<p>Welcome to @lang('general.site_name')! Please click on the following link to confirm your @lang('general.site_name') account:</p>
 
 <p><a href="{!! $activationUrl !!}">{!! $activationUrl !!}</a></p>
 
