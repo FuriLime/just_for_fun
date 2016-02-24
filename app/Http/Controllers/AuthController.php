7e8a9bef536313291bbc54b02c9be85783f06dc8
furilime <sergey.ch.ysbm@gmail.com>
2016-02-24 14:59:35 +0200

    cxhx

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -211,16 +211,16 @@ class AuthController extends JoshController
                 ),
             );
             $infos = $this->mailchimp->call('lists/member-info', $params);
-            foreach($infos as $data){
-//                foreach($data as $merges){
-//                    $user_email = $merges['email'];
+            foreach($infos['data'] as $data){
+                foreach($data as $merges){
+                    $user_email = $merges['email'];
 ////                    foreach($merges as $group){
 ////
 ////                    }
-//                }
+                }
 //                $subemail = $data['email'];
             }
-            dd($infos);
+            dd($data['email']);
 //            dd(member-info('901e50791519fce4886a3e84f2087ff9-us1', '3b2e9de273'));
             if($activate->isUserActivate($userId)){
                 $user = User::find($userId);
