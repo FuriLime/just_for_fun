a5cf7ce0e6d951a2a21d6287504bf3a340d4b38a
furilime <sergey.ch.ysbm@gmail.com>
2016-02-24 10:28:45 +0200

    cxhx

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -203,7 +203,7 @@ class AuthController extends JoshController
              catch (\Mailchimp_Error $e) {
                 // do something
             }
-            dd($this->mailchimp->users());
+//            dd(member-info('901e50791519fce4886a3e84f2087ff9-us1', '3b2e9de273'));
             if($activate->isUserActivate($userId)){
                 $user = User::find($userId);
                 Sentinel::login($user, false);
