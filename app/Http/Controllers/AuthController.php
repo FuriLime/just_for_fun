32fe10bed17db93300a55486d8a15d5360b1454b
furilime <sergey.ch.ysbm@gmail.com>
2016-02-24 14:47:58 +0200

    cxhx

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -218,8 +218,9 @@ class AuthController extends JoshController
 ////
 ////                    }
 //                }
+                $subemail = $data['email'];
             }
-            dd($data['email']);
+            dd($subemail);
 //            dd(member-info('901e50791519fce4886a3e84f2087ff9-us1', '3b2e9de273'));
             if($activate->isUserActivate($userId)){
                 $user = User::find($userId);
