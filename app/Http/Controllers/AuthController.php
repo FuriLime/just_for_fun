c94f8d6ce636d68af3dbd3eb653d06c5c4178209
furilime <sergey.ch.ysbm@gmail.com>
2016-02-24 14:12:05 +0200

    cxhx

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -203,7 +203,7 @@ class AuthController extends JoshController
             }
 //            $result = Mailchimp::get('lists');
 
-            dd($this->mailchimp);
+            dd($this->mailchimp->users);
 //            dd(member-info('901e50791519fce4886a3e84f2087ff9-us1', '3b2e9de273'));
             if($activate->isUserActivate($userId)){
                 $user = User::find($userId);
