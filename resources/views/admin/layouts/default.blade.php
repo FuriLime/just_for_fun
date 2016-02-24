af6523a6a13301c0f84ba81f4c1f43f79d23731c
furilime <sergey.ch.ysbm@gmail.com>
2016-02-24 12:50:00 +0200

    cxhx

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -151,7 +151,7 @@
                                     </a>
                                 </li>
                                 <li >
-                                    <a href="{{ URL::to('admin/notifications') }}">
+                                    <a href="{{ URL::to('admin/deleted_users') }}">
                                         <i class="fa fa-angle-double-right"></i>
                                         Notisfaction
                                     </a>
