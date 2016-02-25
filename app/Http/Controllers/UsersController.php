3f5d3746a8247bf59dc9d963b8297cb569379b7e
furilime <sergey.ch.ysbm@gmail.com>
2016-02-25 11:57:16 +0200

    cxhx

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -827,6 +827,7 @@ class UsersController extends JoshController
         foreach($result['interests'] as $interes){
 //            $user_inter[] = $interes;
             foreach($interes as $inter_name){
+                dd($inter_name);
                 $sdfsdf[] =$inter_name['name'];
                 dd($inter_name);
             }
