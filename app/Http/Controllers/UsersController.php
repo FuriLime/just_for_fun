418421f6c830e32e6c475b51fdb228564ca42f3f
furilime <sergey.ch.ysbm@gmail.com>
2016-02-25 16:11:23 +0200

    cxhx

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -870,7 +870,7 @@ class UsersController extends JoshController
 //        if($group['check']==1){
 //
 //        }
-        dd($_POST);
+        dd($_POST['check']);
 //        die;
         $this->mailchimp->patch("lists/$this->listId/members/$email", [
             'merge_fields' => ['FNAME'=>'Davy', 'LNAME'=>'Jones'],
