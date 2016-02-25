a7478b46669e636cbcbe317ee3d804c6ce9e4771
furilime <sergey.ch.ysbm@gmail.com>
2016-02-25 11:31:57 +0200

    cxhx

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -820,7 +820,9 @@ class UsersController extends JoshController
 //        $result = $this->mailchimp->get('lists/'.$this->listId.'/members/'.$email,[
 //            'fields' => 'id,interests'
 //        ]);
-        $result = $this->mailchimp->get('lists/'.$this->listId.'/interest-categories/d8186972a7/interests/name');
+        $result = $this->mailchimp->get('lists/'.$this->listId.'/interest-categories/d8186972a7/interests',[
+            'fields' => ['interests'=>['name']]
+        ]);
         dd($result->toArray());
         foreach($result['interests'] as $interes){
             $user_inter[] = $interes;
