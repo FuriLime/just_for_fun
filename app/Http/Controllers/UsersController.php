37986123f72474c66639b4393def860059ba80df
furilime <sergey.ch.ysbm@gmail.com>
2016-02-25 10:29:56 +0200

    cxhx

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -817,7 +817,7 @@ class UsersController extends JoshController
         $email = Sentinel::getUser()->email;
 //        dd($email);
 //        $mc = new \Mailchimp\Mailchimp('901e50791519fce4886a3e84f2087ff9-us1');
-        $result = $this->mailchimp->get('lists/'.$this->listId.'/members/'+$email);
+        $result = $this->mailchimp->get('lists/'.$this->listId.'/members/3e0794e957684d33a69d4ed88779bdb0');
         dd($result->toArray());
 //        $email = Sentinel::getUser()->email;
 //        $params = array(
