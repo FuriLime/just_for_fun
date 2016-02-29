f9ddf26c6ceefe21a9d5beb5cf540921c45cf593
furilime <sergey.ch.ysbm@gmail.com>
2016-02-29 15:18:53 +0200

    google auth

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -111,10 +111,9 @@ class EventsController extends Controller {
 			}
 		} else {
 			//create events unregister user
-
+			$start_date_tmp = strtotime("+1 hour");
 			$start_date = date('Y/m/d 19:00');
-            $start_date_tmp = strtotime($start_date) + 60*60;
-			$finish_date = date($start_date, $start_date_tmp);
+			$finish_date = date($start_date, strtotime("+1 hour"));
 			$default_timezone = date_default_timezone_get();
             $timezone_select = self::getTimeZoneSelect();
             $ip = $_SERVER["REMOTE_ADDR"];
