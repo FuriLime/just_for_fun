4da4a05562c484a5db15cdb7364ee9104557bed8
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 09:57:02 +0200

    google auth

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -117,15 +117,15 @@ class EventsController extends Controller {
 			$finish_date = date('Y/m/d 20:00');
             $timezone_select = self::getTimeZoneSelect();
             $my_time_zone='';
-//            if($request->ajax()){
+            if($request->ajax()){
                 $input = Input::all();
                 $my_time_zone = $input['value'];
-//            }
+            }
 //            $input = Input::get('usertimezone');
 
             $ip = $_SERVER["REMOTE_ADDR"];
             $location = GeoIP::getLocation($ip);
-//            $my_time_zone = $input['value'];
+            $my_time_zone = $input['value'];
 			return view('events.create', array(
 				'timezone_select' => $timezone_select,
 				'start_date' => $start_date,
