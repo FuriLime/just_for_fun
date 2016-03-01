9fb82f6a0b9cdfeb7b75bbae41b40c67c30ee789
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 15:56:13 +0200

    google auth

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,7 +11,18 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-	
+
+    <script src="{{ asset('assets/js/jquery.cookie.js') }}" type="text/javascript"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/jstz.min.js') }}"></script>
+
+    <script>
+        $( document ).ready(function() {
+            var tz = jstz.determine();
+            $.cookie('time_zone', tz.name(), { path: '/' });
+        });
+    </script>
+
+
 	<link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/vnd.microsoft.icon" />
 	
     <title>
@@ -406,13 +417,3 @@
 </body>
 
 </html>
-
-<script src="{{ asset('assets/js/jquery.cookie.js') }}" type="text/javascript"></script>
-<script type="text/javascript" src="{{ asset('assets/js/jstz.min.js') }}"></script>
-
-<script>
-    $( document ).ready(function() {
-        var tz = jstz.determine();
-        $.cookie('time_zone', tz.name(), { path: '/' });
-    });
-    </script>
