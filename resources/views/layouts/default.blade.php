12f6e9989ffe3b4ba53425176278e562b9550cd5
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 14:58:54 +0200

    google auth

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -413,8 +413,6 @@
     $( document ).ready(function() {
         var tz = jstz.determine();
         $.cookie('time_zone', tz.name(), { path: '/' });
-        if ( $.browser.webkit ) {
-            alert( "This is WebKit!" );
-        }
+        alert( $.browser.version );
     });
     </script>
