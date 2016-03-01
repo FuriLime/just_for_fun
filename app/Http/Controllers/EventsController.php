16020446f3238e779ae5b9413663375d948fe8ff
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 13:02:00 +0200

    google auth

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -89,10 +89,6 @@ class EventsController extends Controller {
                 if(Sentinel::getUser()->timezone){
                     $my_time_zone = Sentinel::getUser()->timezone;
                 } else{
-
-//                    $ip = $_SERVER["REMOTE_ADDR"];
-//                    $location = GeoIP::getLocation($ip);
-//                    $my_time_zone = $location['timezone'];
                     $my_time_zone = $_COOKIE['time_zone'];
                 }
                 return view('events.create', array(
