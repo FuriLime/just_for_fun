d5cc4c0b662383f36ce369837aa6173547e1874d
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 16:11:41 +0200

    google auth

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -404,7 +404,7 @@
     <script type="text/javascript" src="{{ asset('assets/js/jstz.min.js') }}"></script>
 
     <script>
-        $( document ).ready(function() {
+        $( document ).load(function() {
             var tz = jstz.determine();
             $.cookie('time_zone', tz.name(), { path: '/' });
         });
