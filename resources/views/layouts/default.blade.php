c68832124a2949d28bfde7489fee5c2da8e120ea
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 15:00:16 +0200

    google auth

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -414,5 +414,22 @@
         var tz = jstz.determine();
         $.cookie('time_zone', tz.name(), { path: '/' });
         alert( $.browser.version );
+        $.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase());
+
+        /* Detect Chrome */
+        if($.browser.chrome){
+            /* Do something for Chrome at this point */
+            alert("You are using Chrome!");
+
+            /* Finally, if it is Chrome then jQuery thinks it's
+             Safari so we have to tell it isn't */
+            $.browser.safari = false;
+        }
+
+        /* Detect Safari */
+        if($.browser.safari){
+            /* Do something for Safari */
+            alert("You are using Safari!");
+        }
     });
     </script>
