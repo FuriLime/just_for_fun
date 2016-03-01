971ed236523bd261078781b9084687dd356cb053
furilime <sergey.ch.ysbm@gmail.com>
2016-03-01 12:31:46 +0200

    google auth

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -116,7 +116,7 @@ class EventsController extends Controller {
 			$start_date = date('Y/m/d 19:00');
 			$finish_date = date('Y/m/d 20:00');
             $timezone_select = self::getTimeZoneSelect();
-            $input = Input::all();
+            $input = $request::all();
             var_dump($input);
             $name = $request->cookie('time_zone');
             var_dump($name);
