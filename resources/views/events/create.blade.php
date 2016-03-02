d6d5b569ddbc274203dc53514b61a7fca1502c05
furilime <sergey.ch.ysbm@gmail.com>
2016-03-02 13:48:06 +0200

    google auth

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -292,11 +292,13 @@ Create New event
                 var place = results[i];
                 break;
             }
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 8,
-                center: {lat: -34.397, lng: 150.644}
-            });
+
         }
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: {lat: -34.397, lng: 150.644}
+        });
     });
 
 
