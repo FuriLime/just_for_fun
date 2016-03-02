1ef41b82de155948e033c14ef0514fc127f7e588
furilime <sergey.ch.ysbm@gmail.com>
2016-03-02 15:16:34 +0200

    google auth

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -344,7 +344,7 @@ Create New event
                         }
 
                         if (splits.length >= 4) {
-                            street = splits[0] +splits[1].replace(/(^\s*)|(\s*)$/g, '');
+                            street = splits[0] + ' ' +splits[1].replace(/(^\s*)|(\s*)$/g, '');
 //                            street = splits[0].replace(/(^\s*)|(\s*)$/g, '');
                             $('#street').val(street);
 
