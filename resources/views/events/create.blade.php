5466bc445c3dec16ea455ee1ad272c7b143a1a0d
furilime <sergey.ch.ysbm@gmail.com>
2016-03-02 13:55:23 +0200

    google auth

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -287,6 +287,7 @@ Create New event
     $('#location').change(function () {
         $('.locale').attr('style', 'display:none');
         $('.fields_map').attr('style', 'display:block');
+        console.log(status == google.maps.places.PlacesServiceStatus.OK);
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 var place = results[i];
