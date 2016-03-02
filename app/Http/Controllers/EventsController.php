5a07192f50a27853e6950e40b14ae500cab3ca60
furilime <sergey.ch.ysbm@gmail.com>
2016-03-02 16:52:37 +0200

    google auth

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -433,7 +433,7 @@ class EventsController extends Controller {
                $my_time_zone = $user_time_zone;
            }
         $structure = '<select class="form-control timezone" name="timezone" id="timezone">';
-        $structure .= '<option value="">'.var_dump($_POST).'</option>';
+        $structure .= '<option value="">'.$_POST.'</option>';
 
         foreach ($regions as $mask) {
             $zones = \DateTimeZone::listIdentifiers($mask);
