adc213674b3a4d3a24161abbd166cb5d8409fc5d
furilime <sergey.ch.ysbm@gmail.com>
2016-03-03 16:04:42 +0200

    google auth

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,18 +65,12 @@ return [
         ],
 
         's3' => [
-//            'driver' => 's3',
-//            'key'    => $secrets['CUSTOM']['S3_KEY'],
-//            'secret' => $secrets['CUSTOM']['S3_SECRET'],
-//            'region' => $secrets['CUSTOM']['S3_REGION_BACKUP'],
-//            'bucket' => $secrets['CUSTOM']['S3_BUCKET_BACKUP'],
-
             'driver' => 's3',
-            'key'    => 'AKIAJQIUBOL3BP2GW55A',
-            'secret' => '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',
-            'region' => 'eu-west-1',
-            'bucket' => 'sergey-backups',
-            'sitecontent' => 'sergey-sitecontent',
+            'key'    => $secrets['CUSTOM']['S3_KEY'],
+            'secret' => $secrets['CUSTOM']['S3_SECRET'],
+            'region' => $secrets['CUSTOM']['S3_REGION_BACKUP'],
+            'bucket' => $secrets['CUSTOM']['S3_BUCKET_BACKUP'],
+
         ],
 
         'rackspace' => [
