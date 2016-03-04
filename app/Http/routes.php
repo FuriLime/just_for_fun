1cc7ac290129240353c1c38b7f542a707637f6e0
furilime <sergey.ch.ysbm@gmail.com>
2016-03-04 13:49:17 +0200

    invite system

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -213,7 +213,7 @@ Route::post('blogitem/{blog}/comment', 'BlogController@storeCommentFrontend');
 Route::get('{name?}', 'JoshController@showFrontEndView');
 
 
-Route::get('invite', 'InviteController@create');
+Route::get('auth/invite', 'InviteController@create');
 Route::post('invite', 'InviteController@store');
 Route::get('auth/invite-only', 'InviteController@invitesonly');
 
