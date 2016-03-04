9dcdcc7b7460fcb9413ab190ebcd921f8ab745ef
furilime <sergey.ch.ysbm@gmail.com>
2016-03-04 13:06:14 +0200

    invite system

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -16,7 +16,7 @@ class InviteController extends Controller {
      */
 
     public function __construct() {
-        $this->middleware('auth', ['except' => 'store']);
+        $this->middleware('auth', ['except' => 'create']);
     }
 
     public function create() {
