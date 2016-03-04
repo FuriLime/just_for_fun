ba7ded62319e14ed72a2580f48eec841a07b279b
furilime <sergey.ch.ysbm@gmail.com>
2016-03-04 11:39:50 +0200

    invite system

---
+++ app/Http/Middleware/InviteMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Invite;
+
+class InviteMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+
+        if (!$request->has('code')) {
+            return redirect('auth/invite-only');
+        }
+
+        $code = $request->input('code');
+        //if invite does`t used
+        $invite = Invite::getInviteByCode($code);
+
+
+        if (!$invite) {
+            return redirect('auth/invite-only');
+        }
+
+        //
+        return $next($request);
+    }
+}
