c66073839d7c9734f9ade3f2349622dd46ab2532
furilime <sergey.ch.ysbm@gmail.com>
2016-03-04 11:29:29 +0200

    invite system

---
+++ resources/views/invite/create.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts/default')
+
+{{-- Page title --}}
+@section('title')
+    Products
+    @parent
+@stop
+
+{{-- page level styles --}}
+@section('header_styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/frontend/shopping.css') }}">
+@stop
+
+{{-- breadcrumb --}}
+@section('top')
+    <div class="breadcum">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li>
+                    <a href="{{ route('home') }}"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Dashboard
+                    </a>
+                </li>
+                <li class="hidden-xs">
+                    <i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
+                    <a href="#">Products</a>
+                </li>
+            </ol>
+            <div class="pull-right">
+                <i class="livicon icon3" data-name="edit" data-size="20" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i> Products
+            </div>
+        </div>
+    </div>
+@stop
+
+
+{{-- Page content --}}
+@section('content')
+    <!-- Container Section Start -->
+    <div class="container">
+        <!--Content Section Start -->
+        <!-- Best Deal Section Start -->
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Invite new user</div>
+                    <div class="panel-body">
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        {!! Form::open() !!}
+                        <div class="form-group">
+                            {!! Form::label('email', 'Email:') !!}
+                            {!! Form::text('email', NULL, ['class' => 'form-control']) !!}
+                            {!! Form::label('message', 'Message:') !!}
+                            {!! Form::textarea('message', NULL, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::submit('Send', ['class' => 'btn btn-primary form-control']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop
+
+{{-- page level scripts --}}
+@section('footer_scripts')
+
+@stop
