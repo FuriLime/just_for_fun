4519a933558a1bbb89bb8bd1c35f75fdb91d4e68
furilime <sergey.ch.ysbm@gmail.com>
2016-03-07 18:05:23 +0200

    database

--- app/Event.php
+++ app/Event.php
@@ -1,71 +1,21 @@
-<?php
+<?php namespace App;
 
-namespace App;
-
-use App\Custom\AutomaticUuidKey;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Event extends Model
-{
-    use AutomaticUuidKey, SoftDeletes;
+class Event extends Model  {
 
     /**
-     * The attributes that should be mutated to dates.
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'events';
+
+    /**
+     * Attributes that should be mass-assignable.
      *
      * @var array
      */
-    protected $dates = [            // to make sure dates are treated as instances of Carbon()
-        'deleted_at',
-    	'start',
-    	'finish',
-    	'test_until',
-    	'free_downloads_until',
-    ];
+    protected $fillable = ['uuid', 'title', 'type', 'description', 'location', 'url', 'timezone', 'start', 'finish', 'active'];
 
-
-    /**
-     * Get the account that owns the event.
-     */
-    public function account()
-    {
-        return $this->belongsTo('App\Account');
-    }
-
-
-    /**
-     * Get the author that has created the event.
-     */
-    public function author()
-    {
-        return $this->belongsTo('App\User');
-    }
-    
-
-    /**
-     * Get the editor who last edited the event.
-     */
-    public function last_editor()
-    {
-        return $this->belongsTo('App\User', 'editor_id');
-    }
-
-
-    /**
-     * Get the download transactions for the event.
-     */
-    public function download_transactions()
-    {
-        return $this->hasMany('App\DownloadTransaction');
-    }
-
-
-    /**
-     * Get the event transactions for the event.
-     */
-    public function event_transactions()
-    {
-        return $this->hasMany('App\EventTransaction');
-    }
-    
 }
