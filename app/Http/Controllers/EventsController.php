11e63697003df824e36e688b5d0ac9f8470ac79d
furilime <sergey.ch.ysbm@gmail.com>
2016-03-07 20:03:50 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -67,7 +67,7 @@ class EventsController extends Controller {
         $location = GeoIP::getLocation($ip);
         $pre_timezone = null;
 //        if($location['timezone']!=NULL || $location['timezone']!='') {
-//            $my_time_zone = $location['timezone'];
+            $my_time_zone = $location['timezone'];
 //        }else if(isset($_COOKIE['time_zone'])) {
 //            $my_time_zone = $_COOKIE['time_zone'];
 //        }
