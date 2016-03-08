2a25e8634e6e383e21c6a8adebbe672627195a71
furilime <sergey.ch.ysbm@gmail.com>
2016-03-08 22:22:50 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -238,18 +238,18 @@ Create New event
             minuteStep: 10,
             minDate: date
         });
-        var start_date = new Date($('#start').val());
-        var end_date = new Date(start_date);
-        end_date.setHours(start_date.getHours() + 1);
-        end_date = end_date.format('Y/m/d H:i');
-        $("#datefinish").datetimepicker({
-            format: 'yyyy/mm/dd hh:ii',
-            autoclose: true,
-            todayBtn: true,
-            startDate: end_date,
-            minuteStep: 10,
-            minDate: end_date
-        })
+//        var start_date = new Date($('#start').val());
+//        var end_date = new Date(start_date);
+//        end_date.setHours(start_date.getHours() + 1);
+//        end_date = end_date.format('Y/m/d H:i');
+//        $("#datefinish").datetimepicker({
+//            format: 'yyyy/mm/dd hh:ii',
+//            autoclose: true,
+//            todayBtn: true,
+//            startDate: end_date,
+//            minuteStep: 10,
+//            minDate: end_date
+//        })
         $('#start').on('change', function() {
             var start_def_date = new Date('{{ $start_date }}');
             var start_date = new Date($('#start').val());
