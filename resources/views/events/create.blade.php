7dc8770b8391e5ffb541f77b9e112a83bd4d86c5
furilime <sergey.ch.ysbm@gmail.com>
2016-03-08 22:30:06 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -275,18 +275,18 @@ Create New event
 //    minDate: end_date
 //});
 
-//        $('#finish').on('change', function() {
-//            var start_date = new Date($('#start').val());
-//            var end_date = new Date($('#finish').val());
-//
-//            if(end_date.getTime() < start_date.getTime()) {
-//                var end_date = new Date(start_date);
-//                end_date.setHours(start_date.getHours() + 1);
-//                end_date = end_date.format('Y/m/d H:i');
-//
-//                $('#finish').val(end_date);
-//            }
-//        });
+        $('#finish').on('change', function() {
+            var start_date = new Date($('#start').val());
+            var end_date = new Date($('#finish').val());
+
+            if(end_date.getTime() < start_date.getTime()) {
+                var end_date = new Date(start_date);
+                end_date.setHours(start_date.getHours() + 1);
+                end_date = end_date.format('Y/m/d H:i');
+
+                $('#finish').val(end_date);
+            }
+        });
 
 //    });
     </script>
