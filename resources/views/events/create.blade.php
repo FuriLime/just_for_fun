08cf18dbe647feba4abbcd84f3ec980195554177
furilime <sergey.ch.ysbm@gmail.com>
2016-03-08 21:59:36 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -238,6 +238,42 @@ Create New event
             minuteStep: 10,
             minDate: date
         });
+        $('#start').on('change', function() {
+            var start_def_date = new Date('{{ $start_date }}');
+            var start_date = new Date($('#start').val());
+
+            if(start_date.getTime() < start_def_date.getTime()) {
+                $('#start').val(start_def_date.format('Y/m/d H:i'));
+                return false;
+            }
+
+            var end_date = new Date(start_date);
+            end_date.setHours(start_date.getHours() + 1);
+            end_date = end_date.format('Y/m/d H:i');
+            $('#finish').val(end_date);
+
+            $("#datefinish").datetimepicker({
+                format: 'yyyy/mm/dd hh:ii',
+                autoclose: true,
+                todayBtn: true,
+                startDate: end_date,
+                minDate: end_date,
+                minuteStep: 10
+            });
+        });
+
+        $('#finish').on('change', function() {
+            var start_date = new Date($('#start').val());
+            var end_date = new Date($('#finish').val());
+
+            if(end_date.getTime() < start_date.getTime()) {
+                var end_date = new Date(start_date);
+                end_date.setHours(start_date.getHours() + 1);
+                end_date = end_date.format('Y/m/d H:i');
+
+                $('#finish').val(end_date);
+            }
+        });
         var start_date = new Date($('#start').val());
         var end_date = new Date(start_date);
         end_date.setHours(start_date.getHours() + 1);
@@ -252,10 +288,6 @@ Create New event
             minuteStep: 10
         });
 
-        $('#datestart .glyphicon-remove').click(function(){
-            console.log('dddd');
-            $('#datefinish').val('asaaa');
-        });
     });
     </script>
 	<script type='text/javascript' src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
@@ -518,45 +550,7 @@ if($('#location').val()) {
 		$('#datefinish .glyphicon-calendar').click();
 	});
 
-    $('#start').on('change', function() {
-        var start_def_date = new Date('{{ $start_date }}');
-        var start_date = new Date($('#start').val());
 
-        if(start_date.getTime() < start_def_date.getTime()) {
-            $('#start').val(start_def_date.format('Y/m/d H:i'));
-            return false;
-        }
-
-        var end_date = new Date(start_date);
-        end_date.setHours(start_date.getHours() + 1);
-        end_date = end_date.format('Y/m/d H:i');
-        $('#finish').val(end_date);
-
-        $("#datefinish").datetimepicker({
-            format: 'yyyy/mm/dd hh:ii',
-            autoclose: true,
-            todayBtn: true,
-            startDate: end_date,
-            minDate: end_date,
-            minuteStep: 10
-        });
-    });
-
-    $('#finish').on('change', function() {
-        var start_date = new Date($('#start').val());
-        var end_date = new Date($('#finish').val());
-
-        if(end_date.getTime() < start_date.getTime()) {
-            var end_date = new Date(start_date);
-            end_date.setHours(start_date.getHours() + 1);
-            end_date = end_date.format('Y/m/d H:i');
-
-            $('#finish').val(end_date);
-        }
-
-
-
-    });
 
 	$('input#title').maxlength({
 		//alwaysShow: true,
