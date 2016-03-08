c72c1f8f6812e13890eecc54059e9affb8c9237e
furilime <sergey.ch.ysbm@gmail.com>
2016-03-08 20:39:48 +0200

    database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends JoshController
             {
                 $email= Input::only('email');
                 // Redirect to the dashboard page
-                $user = User::where('email', $email['email']);
+                $user = User::where('email', $email['email'])->get();
                 dd($user);
                 $user = Sentinel::check();
                 $user_email = $user["attributes"]["email"];
