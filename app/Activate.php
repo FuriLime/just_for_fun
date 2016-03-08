d4fdca2ef76bb62b7e34231b5221ee415d058a1c
furilime <sergey.ch.ysbm@gmail.com>
2016-03-08 20:49:07 +0200

    database

--- app/Activate.php
+++ app/Activate.php
@@ -37,7 +37,7 @@ class Activate extends Model {
     public function activateUser($user_id)
     {
         $user = User::find($user_id);
-		$user->isActivate = 0;
+		$user->isActivate = 1;
 		$user->save();
     }
 
