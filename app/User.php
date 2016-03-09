14ea736fb546ced789b2ef2ecd3fbf544bc14b6d
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 16:46:30 +0200

    database

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends EloquentUser {
 	 *
 	 * @var string
 	 */
-	protected $table = 'users';
+//	protected $table = 'users';
 
 	/**
 	 * The attributes excluded from the model's JSON form.
