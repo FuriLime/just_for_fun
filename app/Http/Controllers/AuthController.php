3b667036ba5dac37984965d163f4bf3ab805895c
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 10:13:40 +0200

    database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,7 +69,7 @@ class AuthController extends JoshController
                 $this->messageBag->add('email', Lang::get('auth/message.account_not_activated'));
                 return back()->withInput()->withErrors($this->messageBag);
             }
-        }
+
         try {
             // Try to log the user in
             if(Sentinel::authenticate(Input::only('email', 'password'), Input::get('remember-me', false)) && $activeUser==1)
@@ -88,7 +88,7 @@ class AuthController extends JoshController
             $delay = $e->getDelay();
             $this->messageBag->add('email', Lang::get('auth/message.account_suspended', compact('delay' )));
         }
-
+        }
         // Ooops.. something went wrong
         return back()->withInput()->withErrors($this->messageBag);
     }
