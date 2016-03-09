09bde56bfb797e0a320400b3e56749a1133dada1
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 10:32:07 +0200

    database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends JoshController
         try {
 
             $user = User::where('email', $email['email'])->get();
-            dd($user->items);
+            dd($user['items']);
             if(!empty($user)) {
                 $activeUser = $user['0']['original']['isActivate'];
 //          dd($activeUser);
