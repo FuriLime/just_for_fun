59790cbaab865afa55fd2a04da73ba6c67a46a29
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 12:34:53 +0200

    database

--- app/Event.php
+++ app/Event.php
@@ -22,6 +22,6 @@ class Event extends Model
      *
      * @var array
      */
-    protected $fillable = ['uuid', 'title', 'type', 'description', 'location', 'url', 'timezone', 'start', 'finish', 'active'];
+    protected $fillable = ['uuid', 'title', 'type', 'description', 'location', 'url', 'timezone', 'start', 'finish', 'status'];
     
 }
