fac1e49ea9e9a52af12aa0bd02c6ff5884aa5c1e
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 16:35:45 +0200

    database

---
+++ database/migrations/2016_03_09_143513_create_account_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('account_id')->unsigned();
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+
+            $table->string('timezone')->required();
+            $table->text('about')->nullable();
+            $table->string('image')->nullable();
+            $table->string('homepage')->nullable();
+
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('account_profiles');
+    }
+}
