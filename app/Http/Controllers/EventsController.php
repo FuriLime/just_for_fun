5e7f262d429545983acab43a66623bfa4bcad902
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 22:28:50 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -134,6 +134,9 @@ class EventsController extends Controller {
     ]);
     $store_info = $request->all();
     $store_info['uuid'] = Uuid::uuid4(4);
+      if (Sentinel::check()) {
+          $store_info['author_id'] = Sentinel::getUser()->id;
+          }
     // for bootstrap-datepicker perform "08/10/2015 19:00" to "2015-10-08 19:00"
     $date = new \DateTime($store_info['start'], new \DateTimeZone($store_info['timezone']));
     $date->setTimezone(new \DateTimeZone('UTC'));
