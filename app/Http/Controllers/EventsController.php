ba65e342e68f8136f336f1014aea724380a4eb20
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 22:48:00 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -151,9 +151,10 @@ class EventsController extends Controller {
     // Is the user logged in?
     if (Sentinel::check()) {
       if (Sentinel::inRole('admin') || Sentinel::inRole('user')) {
-          dd($store_info);
+
         event::create($store_info);
           Session::forget('timezone');
+          dd($store_info);
         return redirect('events')->with('success', Lang::get('message.success.create'));
       }
     } else {
