0adad51c8e9225d4f1bd1c7bdda27e90f3f32849
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 12:25:56 +0200

    database

--- database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
+++ database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
@@ -60,29 +60,6 @@ class MigrationCartalystSentinel extends Migration
             $table->timestamps();
         });
 
-// // taken out to a separate migration
-//         Schema::create('roles', function (Blueprint $table) {
-//             $table->increments('id')->unsigned();
-//             $table->string('slug');
-//             $table->string('name');
-//             $table->string('description');
-//             $table->text('permissions')->nullable();        // permissions cannot be stored with the roles in our case
-//             $table->timestamps();
-
-//             $table->engine = 'InnoDB';
-//             $table->unique('slug');
-//         });
-
-// //this table should not exist as role_id is an add on column on account_user table
-//         Schema::create('role_users', function (Blueprint $table) {
-//             $table->integer('user_id');
-//             $table->integer('role_id');
-//             $table->nullableTimestamps();
-
-//             $table->engine = 'InnoDB';
-//             $table->primary(['user_id', 'role_id']);
-//         });
-
         Schema::create('throttle', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->nullable();
@@ -94,22 +71,6 @@ class MigrationCartalystSentinel extends Migration
             $table->index('user_id');
         });
 
-// // taken out to a separate migration
-        // Schema::create('users', function (Blueprint $table) {   //moved to separate migration
-        //     $table->integer('id');
-        //     $table->string('nickname');
-        //     $table->string('email');
-        //     $table->string('password');
-        //     $table->text('permissions')->nullable();
-        //     $table->timestamp('last_login')->nullable();
-        //     $table->string('first_name')->nullable();
-        //     $table->string('last_name')->nullable();
-        //     $table->integer('login_count')->unsigned()->default(0);
-        //     $table->timestamps();
-
-        //     $table->engine = 'InnoDB';
-        //     $table->unique('email');
-        // });
     }
 
     /**
