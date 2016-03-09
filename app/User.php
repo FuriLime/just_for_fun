4cedcbe2804166594d03b3e90f618389008f54d7
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 16:00:30 +0200

    database

--- app/User.php
+++ app/User.php
@@ -38,46 +38,46 @@ class User extends EloquentUser
     {
         return $this->hasOne('App\Activate');
     }
-
-    public function user_profile()
-    {
-        return $this->hasOne('App\UserProfile');
-    }
-
-    /**
-     * The accounts that belong to the user. Containing all Account the user has access to.
-     */
-    public function accounts()
-    {
-        return $this->belongsToMany('App\Account')->withPivot('role_id')->withTimestamps();
-    }
-
-
-    /**
-     * The roles that belong to the user.
-     */
-    public function roles()
-    {
-        return $this->belongsToMany('App\Role', 'role_users')->withPivot('role_id')->withTimestamps();
-    }
+//
+//    public function user_profile()
+//    {
+//        return $this->hasOne('App\UserProfile');
+//    }
+//
+//    /**
+//     * The accounts that belong to the user. Containing all Account the user has access to.
+//     */
+//    public function accounts()
+//    {
+//        return $this->belongsToMany('App\Account')->withPivot('role_id')->withTimestamps();
+//    }
+//
+//
+//    /**
+//     * The roles that belong to the user.
+//     */
+//    public function roles()
+//    {
+//        return $this->belongsToMany('App\Role', 'role_users')->withPivot('role_id')->withTimestamps();
+//    }
 
 
     /**
      * The events that have been created by the user.
      */
-    public function events_author()
-    {
-        return $this->hasMany('App\Event', 'author_id');
-    }
-
-
-    /**
-     * The events where the user was the last editor of the vent.
-     */
-    public function events_last_editor()
-    {
-        return $this->hasMany('App\Event', 'editor_id');
-    }
+//    public function events_author()
+//    {
+//        return $this->hasMany('App\Event', 'author_id');
+//    }
+//
+//
+//    /**
+//     * The events where the user was the last editor of the vent.
+//     */
+//    public function events_last_editor()
+//    {
+//        return $this->hasMany('App\Event', 'editor_id');
+//    }
 
 
     /**
