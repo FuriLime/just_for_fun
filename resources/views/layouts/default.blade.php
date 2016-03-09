a849474c1af613ad0305edf167ac71c8554ec12b
furilime <sergey.ch.ysbm@gmail.com>
2016-03-09 17:16:04 +0200

    database

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -30,6 +30,15 @@
 </head>
 
 <body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KGDVW9&gtm_auth=5wQO1SzIu7QKQs73d13OGw&gtm_preview=env-33&gtm_cookies_win=x"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl+'&gtm_auth=5wQO1SzIu7QKQs73d13OGw&gtm_preview=env-33&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KGDVW9');</script>
+<!-- End Google Tag Manager -->
 <!-- Header Start -->
 <header>
     <!-- Icon Section Start -->
