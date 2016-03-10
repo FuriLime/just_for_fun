995e7950ed8cfefea9741205298252fb70b6f2fe
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-10 08:58:58 +0200

    database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -161,7 +161,7 @@ class AuthController extends JoshController
                 ['name' => 'FNAME',                 'content' => 'User reigester without first name'],
                 ['name' => 'LNAME',                 'content' => 'User reigester without last name'],
                 ['name' => 'LOGINCOUNT',            'content' => 'We not have this data yet'],
-                ['name' => 'PASSRESET',             'content' => $data['activationUrl']],
+                ['name' => 'PASSRESET',             'content' => ''],
                 ['name' => 'RESETVALID',            'content' => 'We not have this data yet'],
                 ['name' => 'DCREDITS',              'content' => '30'],
                 ['name' => 'ECREDITS',              'content' => 'We not have this data yet'],
