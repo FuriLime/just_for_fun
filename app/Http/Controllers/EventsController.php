54057fc010b170e40881fb0c9d6ed6af1e995cb1
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-10 16:28:35 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -149,20 +149,20 @@ class EventsController extends Controller {
 
 //      dd($store_info['author_id']);
     // for bootstrap-datepicker perform "08/10/2015 19:00" to "2015-10-08 19:00"
-    $date = new \DateTime($store_info['start'], new \DateTimeZone($store_info['timezone']));
-    $date->setTimezone(new \DateTimeZone('UTC'));
-    $event_start_zero = $date;
-    $date = new \DateTime($store_info['finish'], new \DateTimeZone($store_info['timezone']));
-    $date->setTimezone(new \DateTimeZone('UTC'));
-    $event_finish_zero = $date;
-
-    // $event['period'] = date($event_start_zero->format('Y-m-d H:i')).' - '.date($event_finish_zero->format('Y-m-d H:i'));
-    $store_info['start'] = $event_start_zero->format('Y-m-d H:i');
-    $store_info['finish'] = $event_finish_zero->format('Y-m-d H:i');
+//    $date = new \DateTime($store_info['start'], new \DateTimeZone($store_info['timezone']));
+//    $date->setTimezone(new \DateTimeZone('UTC'));
+//    $event_start_zero = $date;
+//    $date = new \DateTime($store_info['finish'], new \DateTimeZone($store_info['timezone']));
+//    $date->setTimezone(new \DateTimeZone('UTC'));
+//    $event_finish_zero = $date;
+//
+//    // $event['period'] = date($event_start_zero->format('Y-m-d H:i')).' - '.date($event_finish_zero->format('Y-m-d H:i'));
+//    $store_info['start'] = $event_start_zero->format('Y-m-d H:i');
+//    $store_info['finish'] = $event_finish_zero->format('Y-m-d H:i');
 
     // Is the user logged in?
     if (Sentinel::check()) {
-          dd($store_info);
+//          dd($store_info);
         event::create($store_info);
           Session::forget('timezone');
 
