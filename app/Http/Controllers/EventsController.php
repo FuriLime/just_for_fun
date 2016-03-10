16f0ffa10ba0ac3343defd9d3ff93f1b4885e89c
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-10 14:17:31 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -137,9 +137,9 @@ class EventsController extends Controller {
       $store_info = $request->all();
         $store_info['uuid'] = Uuid::uuid4(4);
       if (Sentinel::check()) {
-          $store_info['author_id'] = '25';
+          $store_info['author_id'] = '28';
           $store_info['editor_id'] = '28';
-          $store_info['account_id'] = '28';
+          $store_info['account_id'] = '25';
           }
 //      dd($store_info['author_id']);
     // for bootstrap-datepicker perform "08/10/2015 19:00" to "2015-10-08 19:00"
