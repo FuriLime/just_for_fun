2bd2bc08bcfb99d484a4f55c8c3dfeee20937772
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 15:56:37 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -272,16 +272,14 @@ Create New event
             var end_date = new Date(start_date);
             end_date.setHours(start_date.getHours() + 1);
             end_date = end_date.format('Y/m/d H:i');
-//console.log(end_date);
-             $('#finish').attr('value', end_date);
+            $('#finish').attr('value', end_date);
             var finaldate = $('#finish').val();
-            console.log(finaldate);
             $("#datefinish").datetimepicker({
                 format: 'yyyy/mm/dd hh:ii',
                 autoclose: true,
                 todayBtn: true,
-                startDate: end_date,
-                minDate: end_date,
+                startDate: finaldate,
+                minDate: finaldate,
                 minuteStep: 10
             });
         });
