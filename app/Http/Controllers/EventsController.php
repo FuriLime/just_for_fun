55d09365fe958a3a017cdc6455b1deed6ed1fb3b
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 11:05:07 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -144,7 +144,7 @@ class EventsController extends Controller {
 
       }else{
           $user = new User();
-          dd($user);
+          dd($user->id);
           $account_user = new Account();
           $account_user->	account_type_id = '1';
           $account_user->name = $user['uuid'];
