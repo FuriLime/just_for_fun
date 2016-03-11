0072b4764eb570d3619cf3ac858aa36477fc4c49
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 15:34:02 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -257,7 +257,7 @@ Create New event
             var start_date = new Date($('#start').val());
             var start_dateNew = start_date.format('Y/m/d H:i');
             var enddate =  $('#start').attr('value', start_dateNew);
-            console.log(start_dateNew);
+            enddate = enddate.format('Y/m/d H:i');
             if(start_date.getTime() < start_def_date.getTime()) {
                 $('#start').val(start_def_date.format('Y/m/d H:i'));
                 return false;
