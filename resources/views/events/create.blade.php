e2c88cbd97c9d8207b6653653ca69a5e24b6a3b7
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 14:36:49 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -239,7 +239,10 @@ Create New event
             minuteStep: 10,
             minDate: nowtimedate
         });
+    });
+    </script>
 
+    <script>
         $('#start').on('change', function() {
             var start_def_date = new Date(nowtimedate);
             var start_date = new Date($('#start').val());
@@ -263,28 +266,28 @@ Create New event
                 minuteStep: 10
             });
         });
-//        $('#time_change').on('click', function() {
-//            var start_def_date = new Date(nowtimedate);
-//            var start_date = new Date($('#start').val());
-//
-//            if(start_date.getTime() < start_def_date.getTime()) {
-//                $('#start').val(start_def_date.format('Y/m/d H:i'));
-//                return false;
-//            }
-//
-//            var end_date = new Date(start_date);
-//            end_date.setHours(start_date.getHours() + 1);
-//            end_date = end_date.format('Y/m/d H:i');
-//            $('#finish').val(end_date);
-//            $("#datefinish").datetimepicker({
-//                format: 'yyyy/mm/dd hh:ii',
-//                autoclose: true,
-//                todayBtn: true,
-//                startDate: end_date,
-//                minDate: end_date,
-//                minuteStep: 10
-//            });
-//        });
+        //        $('#time_change').on('click', function() {
+        //            var start_def_date = new Date(nowtimedate);
+        //            var start_date = new Date($('#start').val());
+        //
+        //            if(start_date.getTime() < start_def_date.getTime()) {
+        //                $('#start').val(start_def_date.format('Y/m/d H:i'));
+        //                return false;
+        //            }
+        //
+        //            var end_date = new Date(start_date);
+        //            end_date.setHours(start_date.getHours() + 1);
+        //            end_date = end_date.format('Y/m/d H:i');
+        //            $('#finish').val(end_date);
+        //            $("#datefinish").datetimepicker({
+        //                format: 'yyyy/mm/dd hh:ii',
+        //                autoclose: true,
+        //                todayBtn: true,
+        //                startDate: end_date,
+        //                minDate: end_date,
+        //                minuteStep: 10
+        //            });
+        //        });
         $('#finish').on('change', function() {
 
             var start_date = new Date($('#start').val());
@@ -298,9 +301,8 @@ Create New event
                 $('#finish').val(end_date);
             }
         });
+        </script>
 
-    });
-    </script>
 	<script type='text/javascript' src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
 	<script type="text/javascript">//<![CDATA[
 
