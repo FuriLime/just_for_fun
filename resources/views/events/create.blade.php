d96b4f3fc82454b0a26c1cb31367da70bc6b2c63
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 15:11:47 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -253,8 +253,9 @@ Create New event
         });
         $('#start').on('change', function() {
             var start_def_date = new Date('{{ $start_date }}');
-            var start_date = new Date($('#start').val());
 
+            var start_date = new Date($('#start').val());
+console.log($('#start').val());
             if(start_date.getTime() < start_def_date.getTime()) {
                 $('#start').val(start_def_date.format('Y/m/d H:i'));
                 return false;
