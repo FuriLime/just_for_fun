155bafe52e1667a51cbeb079b13c3fd5af574a19
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 19:38:27 +0200

    database

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -81,7 +81,7 @@
     </ul>
 </li>
 {{  $user = Sentinel::getUser()}}
-{{$userRoles = $user->roles()->lists('name')->all()->name()}}
+{{$userRoles = $user->roles()->lists('name')->first()}}
 {{--@if(Sentinel::inRole('admin'))--}}
     <li >
         <a href="#">
