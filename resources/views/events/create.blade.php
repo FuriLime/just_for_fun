2bb76fe8b65639dcaeb837fe34ea5773e7fe3bd3
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 15:57:52 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -272,7 +272,7 @@ Create New event
             var end_date = new Date(start_date);
             end_date.setHours(start_date.getHours() + 1);
             end_date = end_date.format('Y/m/d H:i');
-            $('#finish').attr('value', end_date);
+//            $('#finish').attr('value', end_date);
             var finaldate = $('#finish').val();
             $("#datefinish").datetimepicker({
                 format: 'yyyy/mm/dd hh:ii',
