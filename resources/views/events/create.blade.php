707a73747abcb1ab2169fe92170637eb97dceefe
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 15:41:46 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -274,7 +274,7 @@ console.log(start_date);
             end_date.setHours(start_date.getHours() + 1);
             end_date = end_date.format('Y/m/d H:i');
 
-            $('#finish').val(end_date);
+            $('#finish').attr('value', end_date);
             $("#datefinish").datetimepicker({
                 format: 'yyyy/mm/dd hh:ii',
                 autoclose: true,
