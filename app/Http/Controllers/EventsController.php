1676882c08f111b7c60b130b6c73587c20b86c0d
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-11 12:07:10 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -140,6 +140,9 @@ class EventsController extends Controller {
       if(isset($_POST['start'])) {
           session()->put('start', $_POST['start']);
       }
+      if(isset($_POST['finish'])) {
+          session()->put('finish', $_POST['finish']);
+      }
     $this->validate($request, [
       'title' => 'required|max:80',
       'description' => 'max:500',
