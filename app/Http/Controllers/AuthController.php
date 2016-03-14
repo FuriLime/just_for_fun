4f3ca197ea9db5182f76798122728fed2bb16bc3
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-14 20:07:43 +0200

    database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -112,7 +112,7 @@ class AuthController extends JoshController
             'email'            => 'required|email|unique:users',
             'password'         => 'required|between:3,32',
             'my_email'   => 'honeypot',
-            'mu_password'   => 'required|honeytime:5'
+            'my_password'   => 'required|honeytime:5'
         );
 
         // Create a new validator instance from our validation rules
