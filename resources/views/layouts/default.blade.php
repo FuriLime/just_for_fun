1f3f2b35282f84459c98f6981fb57160adf79168
furilime <sergey.ch.ysbm@gmail.com>
2016-03-14 14:17:14 +0200

    database

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -231,11 +231,11 @@
                         <p class="signin button">
                             <input type="submit" class="btn btn-success" value="Sign up" />
                         </p>
-                        <p class="change_link">
-                            <a href="{{ URL::to('singin') }}" class="to_register">
-                                <button type="button" class="btn btn-responsive botton-alignment btn-warning btn-sm">Back</button>
-                            </a>
-                        </p>
+                        {{--<p class="change_link">--}}
+                            {{--<a href="{{ URL::to('singin') }}" class="to_register">--}}
+                                {{--<button type="button" class="btn btn-responsive botton-alignment btn-warning btn-sm">Back</button>--}}
+                            {{--</a>--}}
+                        {{--</p>--}}
                     </form>
                 </div>
                 <div id="forgot" class="animate form">
