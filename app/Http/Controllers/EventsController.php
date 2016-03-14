4ce7d5ab8a9b6669dfefc70f64af52ef3491db0a
furilime <sergey.ch.ysbm@gmail.com>
2016-03-14 16:51:41 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -21,7 +21,7 @@ use App\Role;
 use GeoIP;
 use DB;
 use Spatie\GoogleTagManager;
-use Eluceo;
+
 class EventsController extends Controller {
 
   /**
@@ -453,27 +453,13 @@ class EventsController extends Controller {
 
         case 'Outloock':
             $result = 'success_load';
-            $tz  = 'Europe/Berlin';
-            $dtz = new \DateTimeZone($tz);
-            date_default_timezone_set($tz);
-// 1. Create new calendar
-            $vCalendar = new \Eluceo\iCal\Component\Calendar('www.example.com');
-// 2. Create timezone rule object for Daylight Saving Time
-// 3. Create timezone rule object for Standard Time
-// 5. Create an event
-            $vEvent = new \Eluceo\iCal\Component\Event();
-            $vEvent->setDtStart(new \DateTime($date));
-            $vEvent->setDtEnd(new \DateTime($date_end));
-            $vEvent->setSummary($_GET['desc']);
-// 6. Adding Timezone
-            $vEvent->setUseTimezone(true);
-// 7. Add event to calendar
-            $vCalendar->addComponent($vEvent);
-// 8. Set headers
-            header('Content-Type: text/calendar; charset=utf-8');
-            header('Content-Disposition: attachment; filename="cal.ics"');
-// 9. Output
-            echo $vCalendar->render();
+            $calendar_link = '/assets/ical.php?name='. $event['title'] .
+                '&sd='. $event_start_zero->format('Ymd') .
+                '&st='. $event_start_zero->format('His') .
+                '&fd='. $event_finish_zero->format('Ymd') .
+                '&ft='. $event_finish_zero->format('His') .
+                '&loc='. $event['location'] .
+                '&desc='. $event['description'];
             break;
 
         case 'iCal':
