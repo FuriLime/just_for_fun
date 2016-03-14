c21b3467cfa7372ce983d753466532580c32c0de
furilime <sergey.ch.ysbm@gmail.com>
2016-03-14 17:22:32 +0200

    database

--- app/Http/Controllers/twitterController.php
+++ app/Http/Controllers/twitterController.php
@@ -44,6 +44,7 @@ class twitterController extends Controller
 //        $mc = new Mailchimp($apiKey);
 //        $listId = Config::get('mailchimp.listId');
         $userTwit = Socialite::driver('twitter')->user();
+        dd($userTwit);
        // dd($userTwit->getNickName());
         $user = User::wheretwit_nick($userTwit->getNickName())->first();
         if(!$user){
