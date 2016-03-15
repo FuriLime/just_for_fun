276bc123ff933b6d65b920b4a3ae47032443cc93
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 21:34:10 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -566,7 +566,7 @@ class EventsController extends Controller {
         }
 
         $structure = '<select class="form-control timezone" name="timezone" id="timezone">';
-        $structure .= '<option value="">'.$my_time_zone.'</option>';
+//        $structure .= '<option value="">'.$my_time_zone.'</option>';
 
         foreach ($regions as $mask) {
             $zones = \DateTimeZone::listIdentifiers($mask);
