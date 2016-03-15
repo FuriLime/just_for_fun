ad343848ccec0c5190514f8cc299d121cb762b59
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 09:28:26 +0200

    database

--- app/Http/Controllers/twitterController.php
+++ app/Http/Controllers/twitterController.php
@@ -45,10 +45,10 @@ class twitterController extends Controller
         $listId = Config::get('mailchimp.listId');
         $userTwit = Socialite::driver('twitter')->user();
 
-//        if(empty($userTwit->email))
-//        {
-//            return redirect()->route('welcome');
-//        }
+        if(empty($userTwit->email))
+        {
+            return redirect()->route('welcome');
+        }
         $user = User::wheretwit_nick($userTwit->getNickName())->first();
         if(!$user){
             $user = new User;
