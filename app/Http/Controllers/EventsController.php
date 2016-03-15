f13ec13bd0d0627098e8b15de8b3b96beee8fb9a
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 15:04:05 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -310,31 +310,31 @@ class EventsController extends Controller {
   public function update($uuid, Request $request)
   {
 
-//    $this->validate($request, [
-////      'title' => 'required|max:80',
-//////      'description' => 'required|max:500',
-//////      'type' => 'required',
-////      'location' => 'max:255',
-////        'Country' => 'max:255',
-////        'City' => 'max:255',
-////        'State' => 'max:255',
-////        'Street' => 'max:255',
-////      'event_url' =>'max:255',
-////      'timezone' => 'required',
-////      'start' => 'required',
-////      'finish' => 'required',
-//    ]);
-    //$event = Event::findOrFail($uuid);
+    $this->validate($request, [
+      'title' => 'required|max:80',
+      'description' => 'max:500',
+//      'type' => 'required',
+      'location' => 'max:255',
+        'Country' => 'max:255',
+        'City' => 'max:255',
+        'State' => 'max:255',
+        'Street' => 'max:255',
+      'event_url' =>'max:255',
+      'timezone' => 'required',
+      'start' => 'required',
+      'finish' => 'required',
+    ]);
+//    $event = Event::findOrFail($uuid);
 
     $event = Event::whereUuid($uuid)->first();
     // for bootstrap-datepicker perform "08/10/2015 19:00" to "2015-10-08 19:00"
-//    $store_info = $request->all();
+    $store_info = $request->all();
 //
 //    $event['start'] = str_replace('/','-',$store_info['start']);
 //    $event['finish'] = str_replace('/','-',$store_info['finish']);
       $event['timezone_select'] = self::getTimeZoneSelect($event['timezone']);
     $event['timezone'] =$event['timezone'];
-    $event->update($request->all());
+    $event->update($store_info);
 
     // Is the user logged in?
     if (Sentinel::check()) {
