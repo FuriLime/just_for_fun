114f9fb96bf976778fecc04009a1d0fa92631cbc
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 17:13:13 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -290,11 +290,10 @@ class EventsController extends Controller {
         $date = new \DateTime($event['finish'], new \DateTimeZone($event['timezone']));
         $date->setTimezone(new \DateTimeZone($event['timezone']));
         $event_finish_zero = $date;
-
         $event['timezone_select'] = self::getTimeZoneSelect($event['timezone']);
         // for bootstrap-datepicker
-        $event['start'] = date('Y/m/d H:i', strtotime($event['start']));
-        $event['finish'] = date('Y/m/d H:i', strtotime($event['finish']));
+        $event['start'] = date($event_start_zero->format('Y-m-d H:i'));
+        $event['finish'] = date($event_finish_zero->format('Y-m-d H:i'));
         $event['timezone'] =$event['timezone'];
         return view('events.edit', compact('event'));
     }
@@ -321,7 +320,7 @@ class EventsController extends Controller {
         $event = Event::whereUuid($uuid)->first();
         // for bootstrap-datepicker perform "08/10/2015 19:00" to "2015-10-08 19:00"
         $store_info = $request->all();
-        dd($event);
+
         $event['start'] = str_replace('/','-',$store_info['start']);
         $event['finish'] = str_replace('/','-',$store_info['finish']);
         $event['timezone'] =$event['timezone'];
