cc8c7f0a3abde2183d7c63cddfc519323a607c43
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 16:44:33 +0200

    database

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -2,7 +2,7 @@
 
 {{-- Page title --}}
 @section('title')
-Single Product| Welcome to Josh Frontend
+Confirm Page
 @parent
 @stop
 
@@ -16,25 +16,25 @@ Single Product| Welcome to Josh Frontend
 @stop
 
 {{-- breadcrumb --}}
-@section('top')
-    <div class="breadcum">
-        <div class="container">
-            <ol class="breadcrumb">
-                <li>
-                    <a href="{{ route('home') }}"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Dashboard
-                    </a>
-                </li>
-                <li class="hidden-xs">
-                    <i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
-                    <a href="#">Single Product</a>
-                </li>
-            </ol>
-            <div class="pull-right">
-                <i class="livicon icon3" data-name="edit" data-size="20" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i> Single Product
-            </div>
-        </div>
-    </div>
-@stop
+{{--@section('top')--}}
+    {{--<div class="breadcum">--}}
+        {{--<div class="container">--}}
+            {{--<ol class="breadcrumb">--}}
+                {{--<li>--}}
+                    {{--<a href="{{ route('home') }}"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Dashboard--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="hidden-xs">--}}
+                    {{--<i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>--}}
+                    {{--<a href="#">Single Product</a>--}}
+                {{--</li>--}}
+            {{--</ol>--}}
+            {{--<div class="pull-right">--}}
+                {{--<i class="livicon icon3" data-name="edit" data-size="20" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i> Single Product--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--@stop--}}
 
 
 {{-- Page content --}}
