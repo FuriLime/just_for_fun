be10d24ddd3056d0857490deb64b68b6930a0a9f
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 20:33:01 +0200

    database

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -104,18 +104,6 @@
                         <div class="form-group">
                             <label for="start">@lang('frontend.date')</label>
                             <div class="form-group form_datetime">
-                                <div class="input-group date" id="datestart">
-								<span class="input-group-addon">
-									<span class="glyphicon glyphicon-calendar"></span>
-								</span>
-                                    {!! Form::text('start', null, ['class' => 'form-control', 'id' => 'start', 'style' => 'width: 100%;']) !!}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="start">@lang('frontend.enddate')</label>
-                            <div class="form-group form_datetime">
                                 <div class="input-group date form_datetime3 col-md-12"  id="datestart">
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-remove"></span>
@@ -123,12 +111,33 @@
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-calendar"></span>
                                 </span>
-                                    <input class="form-control" size="16" id="start" name="start" type="datetime" value="">
+                                    <input class="form-control" size="16" id="start" name="start" type="datetime" value="{{$store_info['start']}}">
 
                                 </div>
                                 @if ($errors->first('start'))
                                     <ul class="alert alert-danger">
                                         <li>{{ $errors->first('start') }}</li>
+                                    </ul>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="start">@lang('frontend.enddate')</label>
+                            <div class="form-group form_datetime">
+                                <div class="input-group date form_datetime3 col-md-12" id="datefinish">
+                                             <span class="input-group-addon">
+                                                <span class="glyphicon glyphicon-remove"></span>
+                                            </span>
+                                            <span class="input-group-addon">
+                                                <span class="glyphicon glyphicon-th"></span>
+                                            </span>
+                                    <input class="form-control" size="16" id="finish" name="finish" type="text" value="">
+
+                                </div>
+                                @if ($errors->first('finish'))
+                                    <ul class="alert alert-danger">
+                                        <li>{{ $errors->first('finish') }}</li>
                                     </ul>
                                 @endif
                             </div>
