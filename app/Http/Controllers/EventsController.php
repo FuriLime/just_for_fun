c9e28e7995bd83f24b87f79a77d869edc9cd5abb
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 20:22:08 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -321,6 +321,8 @@ class EventsController extends Controller {
         // for bootstrap-datepicker perform "08/10/2015 19:00" to "2015-10-08 19:00"
         $store_info = $request->all();
 
+        dd($store_info);
+
 //        $event['start'] = str_replace('/','-',$store_info['start']);
 //        $event['finish'] = str_replace('/','-',$store_info['finish']);
 
