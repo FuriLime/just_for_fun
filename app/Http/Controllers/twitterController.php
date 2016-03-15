87a4afe0aad6e932773befa831127022e1f00207
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 08:27:54 +0200

    database

--- app/Http/Controllers/twitterController.php
+++ app/Http/Controllers/twitterController.php
@@ -47,7 +47,7 @@ class twitterController extends Controller
 
         if(empty($userTwit->email))
         {
-            return redirect()->route('home');
+            return back()->withInput();
         }
         $user = User::wheretwit_nick($userTwit->getNickName())->first();
         if(!$user){
