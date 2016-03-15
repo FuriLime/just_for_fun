eb55c11332c61abbc8fe7ec95eeec01ba42caf05
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 19:58:12 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -333,6 +333,7 @@ class EventsController extends Controller {
 
         // for bootstrap-datepicker
         $event['start'] = date($event_start_zero->format('Y-m-d H:i'));
+        dd($event['start']);
         $event['finish'] = date($event_finish_zero->format('Y-m-d H:i'));
 //        $event['timezone'] =$event['timezone'];
         $event->update($request->all());
