1ba6214ab4df7705337c9eb4c60ef00230b29ade
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-15 21:31:55 +0200

    database

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -100,7 +100,7 @@
 
                         <div class="form-group">
                             <label for="timezone">@lang('frontend.timezone')</label>
-                            {!! $event->timezone_select !!}
+                            {{--{!! $event->timezone_select !!}--}}
                         </div>
 
                         <div class="form-group">
