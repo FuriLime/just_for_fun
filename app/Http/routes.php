bf506b41b8c512464c68e647d85d005824455348
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-16 18:10:18 +0200

    database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -168,7 +168,7 @@ Route::get('member', array('as' => 'member_home', 'uses' => 'AccountController@i
 Route::resource('events', 'EventsController');
 Route::get('event/add', 'EventsController@create');
 Route::post('event/add', 'EventsController@store');
-
+Route::get('event/add', 'EventsController@edit');
 Route::get('events/{uuid}/delete', array('as' => 'events.delete', 'uses' => 'EventsController@getDelete'));
 Route::get('events/{uuid}/clone', array('as' => 'events.clone', 'uses' => 'EventsController@cloned'));
 Route::post('events/{uuid}/clone', array('as' => 'events.clone', 'uses' => 'EventsController@clonne'));
