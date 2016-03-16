5590e4c2e33134b2c80a697daf33a45d58da9336
furilime <sergey.ch.ysbm@gmail.com>
2016-03-16 11:11:45 +0200

    database

--- app/Blog.php
+++ app/Blog.php
@@ -10,31 +10,31 @@ class Blog extends Model {
 
     use SoftDeletes;
 
-    protected $dates = ['deleted_at'];
-
-    protected $sluggable = [
-        'build_from' => 'title',
-        'save_to'    => 'slug',
-    ];
-
-    protected $table = 'blogs';
-
-    protected $guarded  = array('id');
-
-    public function comments()
-    {
-        return $this->hasMany('App\BlogComment');
-    }
-    public function category()
-    {
-        return $this->belongsTo('App\BlogCategory');
-    }
-    public function author()
-    {
-        return $this->belongsTo('App\User', 'user_id');
-    }
-    public function getBlogcategoryAttribute()
-    {
-        return $this->category->lists('id');
-    }
+//    protected $dates = ['deleted_at'];
+//
+//    protected $sluggable = [
+//        'build_from' => 'title',
+//        'save_to'    => 'slug',
+//    ];
+//
+//    protected $table = 'blogs';
+//
+//    protected $guarded  = array('id');
+//
+//    public function comments()
+//    {
+//        return $this->hasMany('App\BlogComment');
+//    }
+//    public function category()
+//    {
+//        return $this->belongsTo('App\BlogCategory');
+//    }
+//    public function author()
+//    {
+//        return $this->belongsTo('App\User', 'user_id');
+//    }
+//    public function getBlogcategoryAttribute()
+//    {
+//        return $this->category->lists('id');
+//    }
 }
