e3db3c13b7c054cbe21d923f72c0e3da72d74a3b
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-16 13:07:52 +0200

    database

--- resources/lang/en/pop_over.php
+++ resources/lang/en/pop_over.php
@@ -5,3 +5,15 @@
  * Date: 16.03.2016
  * Time: 13:05
  */
+/**
+ * Language file for item error/success messages
+ *
+ */
+
+return array(
+
+    'title'        => 'Popover title',
+    'content'     => 'Some content in Popover on right',
+
+
+);
