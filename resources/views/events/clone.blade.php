e23056e6d184687140a37ca8055dd4e70b538c6f
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-16 12:20:48 +0200

    database

--- resources/views/events/clone.blade.php
+++ resources/views/events/clone.blade.php
@@ -72,7 +72,7 @@
                         <div class="form-group">
                             <label for="description">@lang('frontend.description')</label>
 {{--                            {!! Form::textarea('description', null, ['class' => 'form-control textarea', 'maxlength' => '500', 'id' => 'description']) !!}--}}
-                            <input class="textarea form-control" type="textarea" size="16" id="description" name="description" type="description", maxlength="500" value="{{$event['description']}}">
+                            <input class="textarea form-control" type="textarea" size="16" id="description" name="description", maxlength="500" value="{{$event['description']}}">
                         </div>
                         <div class="form-group">
                             <label for="location">@lang('frontend.location')</label>
