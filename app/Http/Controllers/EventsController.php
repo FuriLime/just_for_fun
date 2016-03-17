ae53878ffd3bc8862add465267e76146bd91e19a
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-17 13:41:34 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -392,6 +392,7 @@ class EventsController extends Controller {
         $event['account_id'] = $eventold['account_id'];
         if(Sentinel::check()) {
             $userId = Sentinel::getUser()->id;
+            dd($userId);
             $user = User::find($userId);
             $account= DB::table('account_user')->select('account_user.account_id')->where('account_user.user_id', '=', $userId)->get('account_id');
             $event['author_id'] = $userId;
