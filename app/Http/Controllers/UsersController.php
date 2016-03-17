27b713101f0368c83bbf5bf54960f444ce9e4927
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-17 08:14:13 +0200

    database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -526,7 +526,7 @@ class UsersController extends JoshController
             // have and the groups the user wish to have.
             $rolesToAdd    = array_diff($selectedRoles, $userRoles);
             $rolesToRemove = array_diff($userRoles, $selectedRoles);
-//            dd($user);
+            dd($user->accounts()->first()->id);
             $acc_id = $user->accounts()->first()->id;
 
             // Remove the user from groups
