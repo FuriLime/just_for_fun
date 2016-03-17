5a489ed882e9b99bd83848baeb716340cc665a42
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-17 18:20:24 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -295,7 +295,7 @@ class EventsController extends Controller {
         $event['start'] = date($event_start_zero->format('Y-m-d H:i'));
         $event['finish'] = date($event_finish_zero->format('Y-m-d H:i'));
 //        $event['timezone'] =$event['timezone'];
-        $event['duration'] = floor((strtotime($event['finish']) - strtotime($event['start']))/86400);
+        $event['duration'] = (strtotime($event['finish']) - strtotime($event['start']))/3600;
         return view('events.create', compact('event'));
     }
 
