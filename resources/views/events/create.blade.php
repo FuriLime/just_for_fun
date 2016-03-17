d901778735573ce122766071bfe1b9dce1a022bb
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-17 13:09:34 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -259,7 +259,7 @@
         $('#select2-timezone-container').attr('title', '{{$event->timezone}}');
         $('#select2-timezone-container').text('{{$event->timezone}}');
         var asd = $('#select2-timezone-container').attr('value', '{{$event->timezone}}');
-        console.log('{!!$event->timezone!!}');
+        console.log('{!!$event->timezone_select!!}');
         @endif
 
         {{--if('{{$event}}'!= null){--}}
