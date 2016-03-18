557279c7b159a7aed5104c302bdec1c4257fd679
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-18 15:51:47 +0200

    database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <form  method="POST" role="form">
+                <form action=" {{ action('twitterController@oauthtwitter') }}" method="" role="form">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                     <div class="form-group">
                     <label for="subject">Enter your email </label>
