4d21a80f142023f283b8bce79c7752bdc5162281
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-18 13:21:09 +0200

    database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -751,7 +751,7 @@ class UsersController extends JoshController
                 $data = array(
 //                        'user'          => $user,
 //                    'deleteUrl' => URL::route('delete', array('user_id' => $user->id, '?delete_code' => $delete_code)),
-                        'deleteUrl' => baseUrl().'/admin/users/'.$user->id.'/delete?delete_code='.$delete_code,
+                        'deleteUrl' =>'/admin/users/'.$user->id.'/delete?delete_code='.$delete_code,
                 );
  if($_GET) {
                 if ($_GET['delete_code'] == $delete_code) {
