80cfb5baef435377eb0476a578f3fe93f4264bda
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-18 15:36:31 +0200

    database

--- app/Http/Controllers/twitterController.php
+++ app/Http/Controllers/twitterController.php
@@ -44,6 +44,10 @@ class twitterController extends Controller
         $mc = new Mailchimp($apiKey);
         $listId = Config::get('mailchimp.listId');
         $userTwit = Socialite::driver('twitter')->user();
+        if(empty( $userTwit->email))
+        {
+            return redirect()->route('welcome');
+        }
 //        if(isset($_POST)){
 //            dd($_POST);
 //        }
