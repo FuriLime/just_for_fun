20886f1291beecc24a75d1ed4b9aa54f8e73bf48
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-18 09:47:02 +0200

    database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -760,11 +760,11 @@ class UsersController extends JoshController
                         $m->to($user->email, $user->first_name . ' ' . $user->last_name);
                         $m->subject('Hello ' . $user->first_name);
                     });
-//               User::destroy($id);
+               User::destroy($id);
                              // Prepare the success message
                 $success = Lang::get('users/message.success.delete');
 //                return Redirect::route('home')->with('success', $success);
-                return Redirect::route('home')->with('success', 'Message with confirmation link has been sent to '.$user->email.'. Please click on the link in the letter that would activate your account.');
+                return Redirect::route('home')->with('success', 'Message with confirmation link has been sent to '.$user->email.'. Please click on the link in the letter that would delete your account.');
             }
 
             // Delete the user
