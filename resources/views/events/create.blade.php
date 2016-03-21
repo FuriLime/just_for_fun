c79e2aff2b881039c6b84d00613af2c66eb23f2b
furilime <sergey.ch.ysbm@gmail.com>
2016-03-21 10:07:52 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -510,7 +510,7 @@
 
                     if (results[0]) {
                         locale = results[0].formatted_address;
-                        console.log(results[0]['address_components']);
+                        console.log(results[0].formatted_address['city']);
                         splits = locale.replace(/-/g,",");
 
                         splits = splits.split(',');
