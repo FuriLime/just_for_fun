2bbd0345c9e62f1a5923ba11bf0519b8ef17a8ab
furilime <sergey.ch.ysbm@gmail.com>
2016-03-21 11:57:40 +0200

    database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -200,8 +200,8 @@ Route::get('oauthpocket', 'PocketController@index');
 // 	});
 // });
 
-Route::post('welcome', 'MailController@emailSend');
-//Route::post('welcome', array('as' => 'welcome','uses' =>  'MailController@postEmailSend'));
+//Route::post('welcome', 'MailController@emailSend');
+Route::post('welcome', array('as' => 'welcome','uses' =>  'MailController@postEmailSend'));
 
 
 Route::get('/facebook','FacebookController@facebook');
