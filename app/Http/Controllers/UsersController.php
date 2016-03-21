d438188fc5950705da4202f114f35bcf6503fe26
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-21 20:36:36 +0200

    database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -533,7 +533,7 @@ class UsersController extends JoshController
                 'offset' => 10,
                 'count' => 10
             ]);
-            dd($result->toArray());
+            dd($mc);
             $mc->delete("lists/$listId/members/$us_email");
         } catch (UserNotFoundException $e) {
             // Prepare the error message
