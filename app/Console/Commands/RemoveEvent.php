d7f749d31661a77fc3ee378dc4b0821153e8c842
furilime <sergey.ch.ysbm@gmail.com>
2016-03-21 12:20:54 +0200

    database

--- app/Console/Commands/RemoveEvent.php
+++ app/Console/Commands/RemoveEvent.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Events;
+use DB;
 
 class RemoveEvent extends Command
 {
@@ -38,5 +40,12 @@ class RemoveEvent extends Command
     public function handle()
     {
         //
+        $events = Events::whereCreateAt(date(('Y-m-d H:i')))->get();
+
+        foreach($events as $event){
+            if($event->test('1')){
+                DB::table('events')->where('create_at', '=', '2016-03-18 14:29:42')->delete();
+            }
+        }
     }
 }
