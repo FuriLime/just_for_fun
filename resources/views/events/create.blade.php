5c3cdb9ab28f1cf1b4e94330eeb94e4924c4ea4c
furilime <sergey.ch.ysbm@gmail.com>
2016-03-21 09:40:05 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -492,7 +492,7 @@
                 service.textSearch(request, callback);
             }
 
-            function callback(results, status) {
+            geocoder.geocode( { 'address': inputAddress}, function callback(results, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
                     for (var i = 0; i < results.length; i++) {
                         var place = results[i];
