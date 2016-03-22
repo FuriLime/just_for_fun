eb72cea207d43d5c6f55b2199bfb12f045309236
furilime <sergey.ch.ysbm@gmail.com>
2016-03-22 12:21:49 +0200

    database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -617,6 +617,7 @@ class UsersController extends JoshController
             }
 
             // Assign the user to groups
+            foreach ($rolesToAdd as $roleId) {
 
                 $role = Role::find($selectedRoles);
                 $rolew = [
@@ -625,7 +626,7 @@ class UsersController extends JoshController
 
                 $role->users()->attach($rolew);
 //                $role->users()->attach();
-
+            }
 
 $new_email = md5(Input::get('email'));
     $mc->put("lists/$listId/members/$new_email", [
