2253240d3549f7c648a530b09d62471417fe874a
furilime <sergey.ch.ysbm@gmail.com>
2016-03-22 15:09:01 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -455,7 +455,7 @@ class EventsController extends Controller {
             return redirect('events')->with('success', Lang::get('message.success.update'));
         }
     }
-    public function cloned($uuid, $title)
+    public function cloned($title)
     {
 
         if(session()->get('start')) {
@@ -468,7 +468,8 @@ class EventsController extends Controller {
             $event_clone['timezone'] = session()->get('timezone');
         }
         //$event = Event::findOrFail($id);
-        $event_clone = Event::whereUuid($uuid)->first();
+        $event_clone = Event::whereUuid($title)->first();
+//        $title = $event_clone->title;
         $date = new \DateTime($event_clone['start'], new \DateTimeZone('UTC'));
         $date->setTimezone(new \DateTimeZone($event_clone['timezone']));
         $event_start_zero = $date;
