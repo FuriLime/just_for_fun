e82c05fdc71971072312b64adecd938213fcb9b4
furilime <sergey.ch.ysbm@gmail.com>
2016-03-22 15:11:34 +0200

    database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -455,7 +455,7 @@ class EventsController extends Controller {
             return redirect('events')->with('success', Lang::get('message.success.update'));
         }
     }
-    public function cloned($uuid)
+    public function cloned($title)
     {
 
         if(session()->get('start')) {
@@ -468,7 +468,7 @@ class EventsController extends Controller {
             $event_clone['timezone'] = session()->get('timezone');
         }
         //$event = Event::findOrFail($id);
-        $event_clone = Event::whereUuid($uuid)->first();
+        $event_clone = Event::whereTitle($title)->first();
         $date = new \DateTime($event_clone['start'], new \DateTimeZone('UTC'));
         $date->setTimezone(new \DateTimeZone($event_clone['timezone']));
         $event_start_zero = $date;
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -62,7 +62,7 @@ Events List
 						<a href="{{ route('events.edit', $event->uuid) }}">
 							<i class="livicon" data-name="edit" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="edit event"></i>
 						</a>
-                        <a href="{{ route('events.clone', $event->uuid) }}">
+                        <a href="{{ route('events.clone', $event->title) }}">
                             <i class="livicon" data-name="clone" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="clone event"></i>
                         </a>
 						<a href="{{ route('events.confirm-delete', $event->uuid) }}" data-toggle="modal" data-target="#delete_confirm">
