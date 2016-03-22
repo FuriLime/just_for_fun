8d58724b2c2d3b3a59035645505ee5dcad4dc487
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-22 20:50:18 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -443,7 +443,7 @@
             autoclose: true,
             todayBtn: true,
             controlType: 'select',
-            startDate: $('#finish').val(),
+            startDate: end_date,
             minDate: $('#finish').val(),
             minuteStep: 10
 
