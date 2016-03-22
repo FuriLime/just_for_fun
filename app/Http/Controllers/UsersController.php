811930088f1d454fcccbdc80c653480ee376dd76
furilime <sergey.ch.ysbm@gmail.com>
2016-03-22 12:35:41 +0200

    database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -613,6 +613,7 @@ class UsersController extends JoshController
                 $rolew = [
                     0 => ['user_id' => $user->id, 'account_id' => $acc_id],
                 ];
+                dd($rolew);
                 $role->users()->detach($rolew);
 
             }
