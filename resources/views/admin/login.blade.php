316d620cadf74f68726e358acf06aa4e9f35ca96
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-22 23:19:23 +0200

    database

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -42,7 +42,7 @@
                                         {!! $errors->first('email', '<span class="help-block">:message</span>') !!}
                                     </div>
                                 </div>
-                                {!! Honeypot::generate('email', 'my_time') !!}
+                                {!! Honeypot::generate('first_name', 'my_time') !!}
                                 <div class="form-group {{ $errors->first('password', 'has-error') }}">
                                     <label style="margin-bottom:0px;" for="password" class="youpasswd"> <i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#3c8dbc" data-hc="#3c8dbc"></i>
                                         Password
