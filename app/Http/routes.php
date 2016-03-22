670210c2dec41cadb3bc92b1e6ac5b8a037200d4
furilime <sergey.ch.ysbm@gmail.com>
2016-03-22 15:06:24 +0200

    database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -178,7 +178,7 @@ Route::post('event/add', 'EventsController@store');
 
 Route::get('events/{uuid}/delete', array('as' => 'events.delete', 'uses' => 'EventsController@getDelete'));
 Route::get('events/{title}/clone', array('as' => 'events.clone', 'uses' => 'EventsController@cloned'));
-Route::post('events/{uuid}/clone', array('as' => 'events.clone', 'uses' => 'EventsController@clonne'));
+Route::post('events/{title}/clone', array('as' => 'events.clone', 'uses' => 'EventsController@clonne'));
 Route::get('events/{uuid}/confirm-delete', array('as' => 'events.confirm-delete', 'uses' => 'EventsController@getModalDelete'));
 Route::post('event/addtocalendar', array('as' => 'event.addtocalendar', 'uses' => 'EventsController@addToCalendar'));
 
