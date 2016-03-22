28ea3b7629ad31bc91a71cc79f7c47477ab7d260
sergeyc <sergey.ch.ysbm@gmail.com>
2016-03-22 23:35:30 +0200

    database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,9 +53,10 @@ class AuthController extends JoshController
     {
         // Declare the rules for the form validation
         $rules = array(
-            'email'    => 'required|honeypot',
+            'email'    => 'required|email',
             'password' => 'required|between:3,32',
             'my_name'   => 'honeypot',
+            'my_time'   => 'required|honeytime:30'
 
         );
 
