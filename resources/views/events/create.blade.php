23655812e4a5aa75cdfc3a5fa66f3ba800b823df
furilime <sergey.ch.ysbm@gmail.com>
2016-03-22 16:34:14 +0200

    database

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -442,7 +442,7 @@
             todayBtn: true,
             controlType: 'select',
             startDate: $('#finish').val(),
-            minDate:minutes_end,
+            minDate: minutes_end,
             minuteStep: 10
 
         });
