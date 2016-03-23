c5b2d50bb916eb7596d7919fefca140485e1f969
furilime <sergey.ch.ysbm@gmail.com>
2016-03-23 14:05:03 +0200

    database

--- app/Http/Controllers/twitterController.php
+++ app/Http/Controllers/twitterController.php
@@ -49,8 +49,6 @@ class twitterController extends Controller
             $userTwit = Socialite::driver('twitter')->user();
             $user = User::wheretwit_nick($userTwit->getNickName())->first();
         }
-        $userTwit = Socialite::driver('twitter')->user();
-        $user = User::wheretwit_nick($userTwit->getNickName())->first();
         if(!$user){
             if(isset($_GET['email'])){
                 $user = new User;
