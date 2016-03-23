4ad437ee85587eb08f8d6b69b2a7ecd24378cbf9
furilime <sergey.ch.ysbm@gmail.com>
2016-03-23 10:03:35 +0200

    database

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -70,45 +70,7 @@
                                 </p>
                             </form>
                         </div>
-                        <div id="register" class="animate form">
-                            <form action="{{ route('signup') }}" autocomplete="on" method="post" role="form">
-                                <h3 class="black_bg">
-                                    <img src="{{ asset('assets/img/logo.png') }}" alt="josh logo">
-                                    <br>Sign Up</h3>
-                                    <!-- CSRF Token -->
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-                                    <div class="form-group {{ $errors->first('email', 'has-error') }}">
-                                        <label style="margin-bottom:0px;" for="email" class="youmail">
-                                            <i class="livicon" data-name="mail" data-size="16" data-loop="true" data-c="#3c8dbc" data-hc="#3c8dbc"></i>
-                                            E-mail
-                                        </label>
-                                        <input id="email" name="email" required type="email" placeholder="mysupermail@mail.com" value="{!! Input::old('email') !!}" />
-                                        <div class="col-sm-12">
-                                            {!! $errors->first('email', '<span class="help-block">:message</span>') !!}
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group {{ $errors->first('password', 'has-error') }}">
-                                        <label style="margin-bottom:0px;" for="password" class="youpasswd">
-                                            <i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#3c8dbc" data-hc="#3c8dbc"></i>
-                                            Password
-                                        </label>
-                                        <input id="password" name="password" required type="password" placeholder="eg. X8df!90EO" />
-                                        <div class="col-sm-12">
-                                            {!! $errors->first('email_confirm', '<span class="help-block">:message</span>') !!}
-                                        </div>
-                                    </div>
-                                <p class="signin button">
-                                    <input type="submit" class="btn btn-success" value="Sign up" />
-                                </p>
-                                <p class="change_link">
-                                    <a href="{{ URL::to('signin') }}" class="to_register">
-                                        <button type="button" class="btn btn-responsive botton-alignment btn-warning btn-sm">Back</button>
-                                    </a>
-                                </p>
-                            </form>
-                        </div>
                         <div id="forgot" class="animate form">
                             <form action="{{ route('forgot-password') }}" autocomplete="on" method="post" role="form">
                                 <h3 class="black_bg">
