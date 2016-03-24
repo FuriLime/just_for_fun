2661d622ad7855dfa8273ac85bee89a50df9dd5d
furilime <sergey.ch.ysbm@gmail.com>
2016-03-24 13:08:47 +0200

    readable url

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts/default')
 
 {{-- Page title --}}
-@section('title')
-{{ $event['title'] }}
-@parent
-@stop
+{{--@section('title')--}}
+{{--{{ $event['title'] }}--}}
+{{--@parent--}}
+{{--@stop--}}
 
 {{-- page level styles --}}
 @section('header_styles')
@@ -20,6 +20,25 @@
       margin-bottom: 5px
     }
 	</style>
+@stop
+@section('top')
+    <div class="breadcum">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li>
+                    <a href="{{ route('home') }}"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Dashboard
+                    </a>
+                </li>
+                <li class="hidden-xs">
+                    <i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
+                    <a href="{{ URL::to('events') }}">Events</a>
+                </li>
+            </ol>
+            <div class="pull-right">
+                <i class="livicon icon3" data-name="doc-landscape" data-size="20" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i> {{ $event['title'] }}
+            </div>
+        </div>
+    </div>
 @stop
 <div class="share-event-top">
   <div class="container share-container">
@@ -54,25 +73,7 @@
   </div>
 </div>
 {{-- breadcrumb --}}
-@section('top')
-<div class="breadcum">
-	<div class="container">
-		<ol class="breadcrumb">
-			<li>
-				<a href="{{ route('home') }}"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Dashboard
-				</a>
-			</li>
-			<li class="hidden-xs">
-				<i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
-				<a href="{{ URL::to('events') }}">Events</a>
-			</li>
-		</ol>
-		<div class="pull-right">
-			<i class="livicon icon3" data-name="doc-landscape" data-size="20" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i> {{ $event['title'] }}
-		</div>
-	</div>
-</div>
-@stop
+
 
 @section('content')
 <!-- Container Section Start -->
