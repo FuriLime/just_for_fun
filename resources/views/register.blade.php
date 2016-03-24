5d2b5442bb69f21c30e601cad4d88302393a564a
furilime <sergey.ch.ysbm@gmail.com>
2016-03-24 09:50:47 +0200

    database

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -171,13 +171,13 @@
                         </div>
                         <div class="form-group">
                             <div class="col-sm-offset-4 col-sm-8">
-                                <button type="submit" class="btn btn-success btn-primary"><span class="glyphicon glyphicon-ok-sign" aria-hidden="true">
-
-                                    </span> Register</button>
+                                <button type="submit" class="btn btn-success btn-primary">
+                                    <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span> Register</button>
                             </div>
                         </div>
                     </form>
                 </div>
+            </div>
             </div>
             <div class="panel panel-default">
                 <div class="panel-heading">Billing Information</div>
@@ -245,3 +245,5 @@
         window.location.href = '/';
     });
 </script>
+</body>
+</html>
