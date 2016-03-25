e723a9687fe41e93b28dc48ea80b83ebdc4be4d5
furilime <sergey.ch.ysbm@gmail.com>
2016-03-25 14:15:05 +0200

    readable url

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -31,7 +31,7 @@
 
 <body>
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id={{env('GTM')}}"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id={{env('GTM_ID')}}"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
