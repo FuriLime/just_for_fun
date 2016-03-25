7bded2f184a86f1e46e75195cafc4b652bad3074
furilime <sergey.ch.ysbm@gmail.com>
2016-03-25 11:26:22 +0200

    readable url

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -3,7 +3,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach($events as $event)
         <url>
-            <loc>{{ URL::route("events.show", [$event->id]) }}</loc>
+            <loc>{{ URL::route("events.show", [$event->readable_url]) }}</loc>
             <title>{{ $event->title}}</title>
             <lastmod>{{ gmdate(DateTime::W3C, strtotime($event->updated_at)) }}</lastmod>
             <changefreq>daily</changefreq>
