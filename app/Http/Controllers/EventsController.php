5662cbf22a9c2b9bd8ae6336fe89071b06e4913f
furilime <sergey.ch.ysbm@gmail.com>
2016-03-25 15:04:45 +0200

    readable url

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -807,7 +807,7 @@ class EventsController extends Controller {
         $loc = urlencode($event['location']);
         $desc = urlencode($dec_title."\r\n")."-----------------------------------------------------------".
             urlencode("\r\n".$event['description'].
-                "\r\n\n".$dec_footer);
+                "\r\n\n---------------------------------------------------".$dec_footer);
         $title = urlencode($event['title']);
         switch ($calendar) {
 
