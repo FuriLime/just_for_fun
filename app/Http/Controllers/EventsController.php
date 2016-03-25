5ee3c1fc3a51fa84c3c1def679b77639e3b77ee4
furilime <sergey.ch.ysbm@gmail.com>
2016-03-25 14:38:25 +0200

    readable url

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -771,7 +771,8 @@ class EventsController extends Controller {
         $acc_type_name= $acc_type_name[0]->name;
 
         //end
-        $text = "This calendar entry has been created with a " .$acc_type_name." Personal Account from EventFellows";
+
+
         $info = $request->all();
 
         $uuid = $info['uuid'];
@@ -799,7 +800,10 @@ class EventsController extends Controller {
             $duration = $hourDifference.$minutesLeft;
         }
         $result = $error_massage = $calendar_link = '';
-        $desc = $text . urlencode("\r\n".$event['description']);
+        $dec_title = "This calendar entry has been created with a " .$acc_type_name." Personal Account from EventFellows";
+        $dec_footer = "Powered by EventFellows - start creating calendar entries for your own event now. https://eventfellows.com/referrer/{{$event->uuid}} ";
+
+        $desc = urlencode($dec_title) . urlencode("\r\n".$event['description'] . urlencode("\r\n".$dec_footer));
         $loc = urlencode($event['location']);
         $title = urlencode($event['title']);
         switch ($calendar) {
