d7f1f6bb79c7203f8a2afb6bf9c72ce1bc262360
DmitryCherednichenko <dmytro.chr.ysbm@gmail.com>
2016-03-31 15:29:37 +0300

    last

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -34,7 +34,7 @@
 		<div class="col-md-1"></div>
 		<div class="col-md-10" id="add_event">
             <div class="panel panel-primary ">
-                <div class="panel-heading">
+                <!-- <div class="panel-heading">
                     <h4 class="panel-title"> <i class="livicon" data-name="plus-alt" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
                         @if (isset($event))
                             @lang('frontend.edit_event_header')
@@ -45,7 +45,7 @@
                         @endif
 
                     </h4>
-                </div>
+                </div> -->
                 <div class="panel-body">
                     @if (isset($event))
                         <h3 class="primary add_event_section_link">@lang('frontend.edit_event_text')</h3>
