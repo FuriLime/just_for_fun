778ee84a29f460640c5fa740e76b391d86d67a26
furilime <sergey.ch.ysbm@gmail.com>
2016-04-01 10:31:07 +0300

    social buttons

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -80,6 +80,8 @@
                 <a class="a2a_button_evernote"></a>
                 <a class="a2a_button_tumblr"></a>
                 <a class="a2a_button_blogger_post"></a>
+                <a href="http://www.skyrock.com/m/blog/share.php?js=0" class="skysocial-s skyrocksocialshare_square24" title="Share on Skyrock" style="display:inline-block;text-indent:-999em;overflow:hidden;width:24px;height:24px;background:url(http://share.static.skyrock.net/img/api/skyrocksocialshare_square24.png) no-repeat 0 0 transparent;"><p style="display:none;">Share on Skyrock</p></a>
+                <script>(function(){var d=document,id='skyrock-fxlebpx'; if(d.getElementById(id)) return;var e=d.createElement('script');e.id=id;e.async=true;e.src='http://share.static.skyrock.net/js/skyrock_social.min.js';d.getElementsByTagName('body')[0].appendChild(e);}());</script>
             </div>
 
         </div>
