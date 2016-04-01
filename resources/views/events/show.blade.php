a3449300b915b7d92e328ff6b7e4ef2347c1bc52
furilime <sergey.ch.ysbm@gmail.com>
2016-04-01 10:54:32 +0300

    social buttons

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -228,7 +228,7 @@ Try Event Fellows for your own events. Event Fellows Accounts are FREE. <a href=
 
 
             <div class="text-center col-xs-12 share-event">Share this event</div>
-            <div class="col-xs-12 text-center">
+            <div class="col-xs-12 social-holder text-center">
                 <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
                     <a class="a2a_button_facebook"></a>
                     <a class="a2a_button_twitter"></a>
