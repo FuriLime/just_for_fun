dce40a2954efbcae9976dc9badbd4aca47d2c7c4
furilime <sergey.ch.ysbm@gmail.com>
2016-04-01 17:41:40 +0300

    social buttons

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -677,7 +677,7 @@
                     var address = document.getElementById('location').value;
                     geocoder.geocode({'address': address}, function(results, status) {
                         if (status == google.maps.GeocoderStatus.OK) {
-                            if (results[1]) {
+                            if (results[0]) {
                                 var loc = getCityState(results);
                                 console.log(loc);
                             }
