cafc944f93853ebbe72728a4616d33546215c35c
furilime <sergey.ch.ysbm@gmail.com>
2016-04-01 17:07:19 +0300

    social buttons

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -596,7 +596,7 @@
                         splits = locale.replace(/-/g,",");
 
                         splits = splits.split(',');
-console.log(locale);
+console.log(results.types);
                         if (splits.length == 2) {
                             sity = splits[0].replace(/(^\s*)|(\s*)$/g, '');
                             $('#city').val(sity);
