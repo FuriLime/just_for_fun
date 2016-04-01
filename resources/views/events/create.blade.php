66af1db94015ce55cf677bbc97e80ca2a017bba3
furilime <sergey.ch.ysbm@gmail.com>
2016-04-01 17:02:53 +0300

    social buttons

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -591,7 +591,7 @@
                     var num_house = '';
                     var asdas = '';
                     var test = place.address_components;
-                    console.log(place.country);
+                    console.log(place.formatted_address);
                     if (results[0]) {
                         locale = results[0].formatted_address;
 
