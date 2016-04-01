1ad41282162973c5e145de53228e24c2f0d1bb93
furilime <sergey.ch.ysbm@gmail.com>
2016-04-01 13:10:54 +0300

    social buttons

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -107,8 +107,8 @@
                         </div>
                         <div class="form-group">
                             <div class="col-sm-offset-4 col-sm-8">
-                                <button type="submit" class="btn btn-success btn-primary">
-                                    <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span> Register</button>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-check-circle"></i>Register</button>
                             </div>
                         </div>
                     </form>
