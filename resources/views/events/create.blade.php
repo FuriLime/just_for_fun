507f2490d1608e19714cf90fe77a86f411759e2a
sergeyc <sergey.ch.ysbm@gmail.com>
2016-04-02 11:14:37 +0300

    social buttons

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -618,14 +618,14 @@
                                         $('#city').attr('value', components.locality);
                                         $('#state').attr('value', components.administrative_area_level_1);
                                         $('#country').attr('value', components.country);
-                                    }else if(components.street_number == undefined && components.route!= undefined
+                                    }elseif(components.street_number == undefined && components.route!= undefined
                                             && components.locality!=undefined && components.administrative_area_level_1 !=undefined
                                             && components.country!=undefined ){
                                         $('#street').attr('value', components.route);
                                         $('#city').attr('value', components.locality);
                                         $('#state').attr('value', components.administrative_area_level_1);
                                         $('#country').attr('value', components.country);
-                                    }else if(components.street_number == undefined && components.route== undefined
+                                    }elseif(components.street_number == undefined && components.route== undefined
                                             && components.locality!=undefined && components.administrative_area_level_1 !=undefined
                                             && components.country!=undefined ){
                                         $('#street').attr('style', 'display:none');
@@ -633,7 +633,7 @@
                                         $('#state').attr('value', components.administrative_area_level_1);
                                         $('#country').attr('value', components.country);
                                     }
-                                    else if(components.street_number == undefined && components.route== undefined
+                                    elseif(components.street_number == undefined && components.route== undefined
                                             && components.locality==undefined && components.administrative_area_level_1 !=undefined
                                             && components.country!=undefined ){
                                         $('#street').attr('style', 'display:none');
