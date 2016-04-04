46b8bb0b4b1338e54f065fd78829bf734a1df082
furilime <sergey.ch.ysbm@gmail.com>
2016-04-04 14:41:53 +0300

    sdd

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -70,15 +70,15 @@
     </div>
     
   </div>
-<!-- @if(Sentinel::check() && $event->author_id == Sentinel::getUser()->id) -->
+@if(Sentinel::check() && $event->author_id == Sentinel::getUser()->id)
         <div class="show-details" id="show_detail">
             Share this event
         </div>
-<!--     @else
-        <div class="show-details" style="display: none">
+    @else
+        <div class="show-details">
             Share this event
         </div>
-@endif -->
+@endif
 </div>
 {{-- breadcrumb --}}
 
