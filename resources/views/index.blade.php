c4606ef4b90adb05c2d08d266ad0f8041b570d02
X <mykola.d.ysbm@gmail.com>
2016-04-06 15:57:36 +0300

    social buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -157,12 +157,12 @@ Home
             </div>
         </div>
     </div>
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <div id="home-carousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            <li data-target="#home-carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#home-carousel" data-slide-to="1"></li>
+            <li data-target="#home-carousel" data-slide-to="2"></li>
         </ol>
 
         <!-- Wrapper for slides -->
@@ -179,7 +179,12 @@ Home
                     ...
                 </div>
             </div>
-            ...
+            <div class="item">
+                <img src="..." alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
         </div>
 
         <!-- Controls -->
