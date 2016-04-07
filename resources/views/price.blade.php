20857ae21eca4d3a5066faa92681908c0ba2f459
X <mykola.d.ysbm@gmail.com>
2016-04-07 17:08:25 +0300

    social buttons

--- resources/views/price.blade.php
+++ resources/views/price.blade.php
@@ -15,28 +15,6 @@ Price
     <!--end of page level css-->
 @stop
 
-{{-- breadcrumb --}}
-@section('top')
-    <div class="breadcum">
-        <div class="container">
-            <ol class="breadcrumb">
-                <li>
-                    <a href="{{ route('home') }}"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Dashboard
-                    </a>
-                </li>
-                <li class="hidden-xs">
-                    <i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
-                    <a href="#">Pricing</a>
-                </li>
-            </ol>
-            <div class="pull-right">
-                <i class="livicon icon3" data-name="money" data-size="20" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i> Pricing
-            </div>
-        </div>
-    </div>
-    @stop
-
-
 {{-- Page content --}}
 @section('content')
     <!-- Container Section Start -->
