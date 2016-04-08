6ee7842f5e9ca7e3b052c677c1d7c965544f88d0
X <mykola.d.ysbm@gmail.com>
2016-04-08 16:31:09 +0300

    social buttons

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -99,13 +99,13 @@ events List
                 </div>
                 <!-- /.tab-pane -->
                 <div class="tab-pane" id="tab_2">
-                    It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                    distribution of letters, as opposed to using 'Content here, content here', making it look like
-                    readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
-                    their default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-                    their infancy. Various versions have evolved over the years, sometimes by accident, sometimes
-                    on purpose (injected humour and the like).
+                    Channel Details Tab Content
+                </div>
+                <div class="tab-pane" id="tab_3">
+                    Team Members Tab Content
+                </div>
+                <div class="tab-pane" id="tab_4">
+                    Channel Settings Tab Content
                 </div>
                 <!-- /.tab-pane -->
             </div>
