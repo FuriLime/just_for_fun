5d0ea10128334ad9fb30979a7abf24630251cbf0
X <mykola.d.ysbm@gmail.com>
2016-04-08 13:31:42 +0300

    social buttons

--- resources/views/price.blade.php
+++ resources/views/price.blade.php
@@ -423,6 +423,7 @@ Price
     </div>
     <div class="feature-comparison-block">
         <div class="container">
+            <div class="feature-comparison-block-title">Commonly asked questions</div>
             <div class="table-responsive">
                 <table class="table feature-comparison-table">
                     <thead>
