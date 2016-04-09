c25e17f2a9420d7c1f378aa9c2b98a8e2fc1fcf2
sergeyc <sergey.ch.ysbm@gmail.com>
2016-04-09 12:52:23 +0300

    social buttons

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -23,89 +23,87 @@
 </div>
 
 <div class="container">
-        <div class="row vertical-offset-100">
-            <!-- Notifications -->
-            @include('notifications')
-
+    <div class="row vertical-offset-100">
+        <!-- Notifications -->
+        @include('notifications')
             <div class="sign-up-form col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
-                        <div class="panel-heading">Login</div>
-                        <div class="panel-body">
-                            <form action="{{ route('signin') }}" autocomplete="on" method="post" role="form">
-                                    <!-- CSRF Token -->
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-
-                                <div class="form-group {{ $errors->first('email', 'has-error') }}">
-                                    <label for="email" class="col-sm-4 control-label">E-Mail Address</label>
-                                    <div class="col-sm-6">
-                                        <input type="email" name="email" required class="form-control" type="email" autocomplete="off" id="email-address" value="{!! Input::old('email') !!}">
+                    <div class="panel-heading">Login</div>
+                    <div class="panel-body">
+                        <form action="{{ route('signin') }}" autocomplete="on" method="post" role="form">
+                            <!-- CSRF Token -->
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                            <div class="form-group {{ $errors->first('email', 'has-error') }}">
+                                <label for="email" class="col-sm-4 control-label">E-Mail Address</label>
+                                <div class="col-sm-6">
+                                    <input type="email" name="email" required class="form-control" type="email" autocomplete="off" id="email-address" value="{!! Input::old('email') !!}">
                                         {!! $errors->first('email', '<span class="help-block">:message</span>') !!}
                                         {!! $errors->first('my_name', '<span class="help-block">:message</span>') !!}
+                                </div>
+                            </div>
+                            {!! Honeypot::generate('my_name', 'my_time') !!}
+                            <div class="form-group {{ $errors->first('password', 'has-error') }}">
+                                <label for="password" class="col-sm-4 control-label">Password</label>
+                                <div class="col-sm-6">
+                                    <input class="form-control" id="password" name="password" autocomplete="off" required type="password" placeholder="eg. X8df!90EO">
+                                    {!! $errors->first('password', '<span class="help-block">:message</span>') !!}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-4 col-sm-6">
+                                    <div class="checkbox">
+                                        <input type="checkbox" name="remember-me" id="remember-me" value="remember-me" style="margin-left: 0">
+                                        <label for="remember-me">Keep me logged in</label>
                                     </div>
                                 </div>
-                                {!! Honeypot::generate('my_name', 'my_time') !!}
+                            </div>
 
-                                <div class="form-group {{ $errors->first('password', 'has-error') }}">
-                                    <label for="password" class="col-sm-4 control-label">Password</label>
-                                    <div class="col-sm-6">
-                                        <input class="form-control" id="password" name="password" autocomplete="off" required type="password" placeholder="eg. X8df!90EO">
-                                        {!! $errors->first('password', '<span class="help-block">:message</span>') !!}
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="col-sm-offset-4 col-sm-6">
-                                        <div class="checkbox">
-                                                <input type="checkbox" name="remember-me" id="remember-me" value="remember-me" style="margin-left: 0">
-                                                <label for="remember-me">Keep me logged in</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div class="form-group">
-                                    <div class="col-sm-offset-4 col-sm-6">
-                                        <a href="{{ URL::to('facebook') }}" class="btn btn-block btn-social btn-facebook">
-                                            <i class="fa fa-facebook"></i>
-                                            Sign in with Facebook
-                                        </a>
-                                        <a href="{{ URL::to('twitter') }}" class="btn btn-block btn-social btn-twitter">
-                                            <i class="fa fa-twitter"></i>
-                                            Sign in with Twitter
-                                        </a>
-                                        <a href="{{ URL::to('google') }}" class="btn btn-block btn-social btn-google-plus">
-                                            <i class="fa fa-google-plus"></i>
-                                            Sign in with Google
-                                        </a>
-                                        <a href="{{ URL::to('linked') }}" class="btn btn-block btn-social btn-linkedin">
-                                            <i class="fa fa-linkedin"></i>
-                                            Sign in with LinkedIn
-                                        </a>
-                                        <a href="{{ URL::to('oauthwindows') }}" class="btn btn-block btn-social btn-linkedin">
-                                            <i class="fa fa-microsoft"></i>
-                                            Sign in with Microsoft
-                                        </a>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="col-sm-offset-4 col-sm-6">
-                                        <button type="submit" class="btn btn-primary">
-                                            <i class="fa fa-check-circle"></i>Log In</button>
-                                    </div>
-                                </div>
-                                <p class="change_link">
-                                    <a href="{{ URL::to('forgot-password') }}">
-                                        <button type="button" class="btn btn-responsive botton-alignment btn-warning btn-sm">Forgot password</button>
+                            <div class="form-group">
+                                <div class="col-sm-offset-4 col-sm-6">
+                                    <a href="{{ URL::to('facebook') }}" class="btn btn-block btn-social btn-facebook">
+                                        <i class="fa fa-facebook"></i>
+                                        Sign in with Facebook
                                     </a>
-                                    <a href="{{ URL::to('signup') }}">
-                                        <button type="button" class="btn btn-responsive botton-alignment btn-success btn-sm" style="float:right;">Sign up</button>
+                                    <a href="{{ URL::to('twitter') }}" class="btn btn-block btn-social btn-twitter">
+                                        <i class="fa fa-twitter"></i>
+                                        Sign in with Twitter
                                     </a>
-                                </p>
-                            </form>
-                        </div>
+                                    <a href="{{ URL::to('google') }}" class="btn btn-block btn-social btn-google-plus">
+                                        <i class="fa fa-google-plus"></i>
+                                        Sign in with Google
+                                    </a>
+                                    <a href="{{ URL::to('linked') }}" class="btn btn-block btn-social btn-linkedin">
+                                        <i class="fa fa-linkedin"></i>
+                                        Sign in with LinkedIn
+                                    </a>
+                                    <a href="{{ URL::to('oauthwindows') }}" class="btn btn-block btn-social btn-linkedin">
+                                        <i class="fa fa-microsoft"></i>
+                                        Sign in with Microsoft
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-offset-4 col-sm-6">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-check-circle"></i>Log In</button>
+                                </div>
+                            </div>
+                            <p class="change_link">
+                                <a href="{{ URL::to('forgot-password') }}">
+                                    <button type="button" class="btn btn-responsive botton-alignment btn-warning btn-sm">Forgot password</button>
+                                </a>
+                                <a href="{{ URL::to('signup') }}">
+                                    <button type="button" class="btn btn-responsive botton-alignment btn-success btn-sm" style="float:right;">Sign up</button>
+                                </a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
 
 
     <!-- global js -->
