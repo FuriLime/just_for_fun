30b05038aba495d33b02ba4bf7be2f12563ac6ec
X <mykola.d.ysbm@gmail.com>
2016-04-11 10:14:29 +0300

    social buttons

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -74,13 +74,13 @@ events List
                                                 </button>
                                                 <ul class="dropdown-menu">
                                                     <li><a href="#"><i class="fa fa-share-alt"></i> Share Event</a></li>
-                                                    <li><a href="#"><i class="fa fa-desktop"></i> Open EventPage</a></li>
+                                                    <li><a href="{{ route('events.show', $event->readable_url) }}"><i class="fa fa-desktop"></i> Open EventPage</a></li>
                                                     <li><a href="#"><i class="fa fa-signal"></i> View Statistics</a></li>
-                                                    <li><a href="#"><i class="fa fa-edit"></i> Edit Event</a></li>
-                                                    <li><a href="#"><i class="fa fa-copy"></i> Duplicate Event</a></li>
+                                                    <li><a href="{{ route('events.edit', $event->readable_url) }}"><i class="fa fa-edit"></i> Edit Event</a></li>
+                                                    <li><a href="{{ route('events.clone', $event->readable_url) }}"><i class="fa fa-copy"></i> Duplicate Event</a></li>
                                                     <li><a href="#"><i class="fa fa-eye"></i> Publish Event</a></li>
                                                     <li><a href="#"><i class="fa fa-eye-slash"></i> Unpublish Event</a></li>
-                                                    <li><a href="#"><i class="fa fa-trash-o"></i> Delete Event</a></li>
+                                                    <li><a href="{{ route('events.confirm-delete', $event->uuid) }}"><i class="fa fa-trash-o"></i> Delete Event</a></li>
                                                 </ul>
                                             </div>
                                         </td>
