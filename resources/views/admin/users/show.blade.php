d62668484d010faac207d11c5dcf92fe647e4516
X <mykola.d.ysbm@gmail.com>
2016-04-11 11:04:54 +0300

    social buttons

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -43,78 +43,80 @@ View User Details
         <br />
         <div class="row">
             <div class="col-lg-12">
-                <ul class="nav  nav-tabs ">
-                    <li class="active">
-                        <a href="#tab1" data-toggle="tab">User Profile</a>
-                    </li>
-                    <li>
-                        <a href="#tab2" data-toggle="tab">
-                            {{--<i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#000" data-hc="#000"></i>--}}
-                            Profile Settings</a>
-                    </li>
-                </ul>
-                <div  class="tab-content mar-top">
-                    <div id="tab1" class="tab-pane fade active in">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="panel">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title">
-                                            User Profile
-                                        </h3>
+                <div class="nav-tabs-custom">
+                    <ul class="nav  nav-tabs ">
+                        <li class="active">
+                            <a href="#tab1" data-toggle="tab">User Profile</a>
+                        </li>
+                        <li>
+                            <a href="#tab2" data-toggle="tab">
+                                {{--<i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#000" data-hc="#000"></i>--}}
+                                Profile Settings</a>
+                        </li>
+                    </ul>
+                    <div  class="tab-content mar-top">
+                        <div id="tab1" class="tab-pane fade active in">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="panel">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">
+                                                User Profile
+                                            </h3>
 
-                                    </div>
-                                    <div class="panel-body">
-                                        <div class="col-md-12">
-                                            <div class="panel-body">
-                                                <div class="table-responsive">
-                                                    <table class="table table-bordered table-striped" id="users">
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="col-md-12">
+                                                <div class="panel-body">
+                                                    <div class="table-responsive">
+                                                        <table class="table table-bordered table-striped" id="users">
+                                                                <tr>
+                                                                    <td>Profile Image</td>
+                                                                    <td>
+                                                                        @if($user->pic)
+                                                                            <img src="{!! url('/').'/uploads/users/'.Sentinel::getUser()->pic !!}" alt="img" class="img-circle img-bor"/>
+                                                                        @else
+                                                                            <img src="{!! asset('assets/img/authors/avatar3.jpg') !!}" class="img-responsive img-circle" alt="User Image">
+                                                                        @endif
+                                                                    </td>
+                                                                </tr>
                                                             <tr>
-                                                                <td>Profile Image</td>
+                                                                <td>@lang('users/title.first_name')</td>
                                                                 <td>
-                                                                    @if($user->pic)
-                                                                        <img src="{!! url('/').'/uploads/users/'.Sentinel::getUser()->pic !!}" alt="img" class="img-circle img-bor"/>
-                                                                    @else
-                                                                        <img src="{!! asset('assets/img/authors/avatar3.jpg') !!}" class="img-responsive img-circle" alt="User Image">
-                                                                    @endif
+                                                                    {{ $user->first_name }}
+                                                                </td>
+
+                                                            </tr>
+                                                            <tr>
+                                                                <td>@lang('users/title.last_name')</td>
+                                                                <td>
+                                                                    {{ $user->last_name }}
+                                                                </td>
+
+                                                            </tr>
+                                                            <tr>
+                                                                <td>@lang('users/title.email')</td>
+                                                                <td>
+                                                                    {{ $user->email }}
                                                                 </td>
                                                             </tr>
-                                                        <tr>
-                                                            <td>@lang('users/title.first_name')</td>
-                                                            <td>
-                                                                {{ $user->first_name }}
-                                                            </td>
+                                                            <tr>
+                                                                <td>Password</td>
+                                                                <td>
+                                                                    {{ $user->password }}
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Type Account</td>
+                                                                <td>
+                                                                    {{ $user->password }}
 
-                                                        </tr>
-                                                        <tr>
-                                                            <td>@lang('users/title.last_name')</td>
-                                                            <td>
-                                                                {{ $user->last_name }}
-                                                            </td>
+                                                                </td>
 
-                                                        </tr>
-                                                        <tr>
-                                                            <td>@lang('users/title.email')</td>
-                                                            <td>
-                                                                {{ $user->email }}
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Password</td>
-                                                            <td>
-                                                                {{ $user->password }}
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Type Account</td>
-                                                            <td>
-                                                                {{ $user->password }}
+                                                            </tr>
 
-                                                            </td>
-
-                                                        </tr>
-
-                                                    </table>
+                                                        </table>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -122,50 +124,50 @@ View User Details
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div id="tab2" class="tab-pane fade">
-                        <div class="row">
-                            <div class="col-md-12 pd-top">
-                                <form action="#" class="form-horizontal">
-                                    <div class="form-body">
-                                        <div class="form-group">
-                                            <label for="inputpassword" class="col-md-3 control-label">
-                                                Password
-                                                <span class='require'>*</span>
-                                            </label>
-                                            <div class="col-md-9">
-                                                <div class="input-group">
-                                                            <span class="input-group-addon">
-                                                                <i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#000" data-hc="#000"></i>
-                                                            </span>
-                                                    <input type="password"  id="inputpassword" placeholder="Password" class="form-control"/>
+                        <div id="tab2" class="tab-pane fade">
+                            <div class="row">
+                                <div class="col-md-12 pd-top">
+                                    <form action="#" class="form-horizontal">
+                                        <div class="form-body">
+                                            <div class="form-group">
+                                                <label for="inputpassword" class="col-md-3 control-label">
+                                                    Password
+                                                    <span class='require'>*</span>
+                                                </label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group">
+                                                                <span class="input-group-addon">
+                                                                    <i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#000" data-hc="#000"></i>
+                                                                </span>
+                                                        <input type="password"  id="inputpassword" placeholder="Password" class="form-control"/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="inputnumber" class="col-md-3 control-label">
+                                                    Confirm Password
+                                                    <span class='require'>*</span>
+                                                </label>
+                                                <div class="col-md-9">
+                                                    <div class="input-group">
+                                                                <span class="input-group-addon">
+                                                                    <i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#000" data-hc="#000"></i>
+                                                                </span>
+                                                        <input type="password"  id="inputnumber"placeholder="Password" class="form-control"/>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="form-group">
-                                            <label for="inputnumber" class="col-md-3 control-label">
-                                                Confirm Password
-                                                <span class='require'>*</span>
-                                            </label>
-                                            <div class="col-md-9">
-                                                <div class="input-group">
-                                                            <span class="input-group-addon">
-                                                                <i class="livicon" data-name="key" data-size="16" data-loop="true" data-c="#000" data-hc="#000"></i>
-                                                            </span>
-                                                    <input type="password"  id="inputnumber"placeholder="Password" class="form-control"/>
-                                                </div>
-                                            </div>
+                                        <div class="form-actions">
+                                            <div class="col-md-offset-3 col-md-9">
+                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                                &nbsp;
+                                                <button type="button" class="btn btn-danger">Cancel</button>
+                                                &nbsp;
+                                                <input type="reset" class="btn btn-default hidden-xs" value="Reset"></div>
                                         </div>
-                                    </div>
-                                    <div class="form-actions">
-                                        <div class="col-md-offset-3 col-md-9">
-                                            <button type="submit" class="btn btn-primary">Submit</button>
-                                            &nbsp;
-                                            <button type="button" class="btn btn-danger">Cancel</button>
-                                            &nbsp;
-                                            <input type="reset" class="btn btn-default hidden-xs" value="Reset"></div>
-                                    </div>
-                                </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
