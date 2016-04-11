af484227c6777b2552c30f458be46712204887c7
X <mykola.d.ysbm@gmail.com>
2016-04-11 10:43:27 +0300

    social buttons

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -38,7 +38,7 @@ events List
 
             <div class="tab-content events-tab-content">
                 <div class="tab-pane active" id="tab_1">
-                    <div class="table-scrollable events-table">
+                    <div class="table-scrollable table-responsive events-table">
                         <table class="table table-hover">
                             <thead>
                                 <tr>
