b9e2e0849b0853657221374370cdb2aa8710c471
X <mykola.d.ysbm@gmail.com>
2016-04-11 10:16:11 +0300

    social buttons

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -90,64 +90,64 @@ events List
                             </tbody>
                         </table>
                     </div>
-                    <table class="table table-bordered " id="table">
-                        <thead>
-                        <tr class="filters">
-                            <th>@lang('frontend.title')</th>
-                            <th>@lang('frontend.type')</th>
-                            <th>@lang('frontend.location')</th>
-                            <th>Event Time Zone</th>
-                            <th>@lang('frontend.start')</th>
-                            <th>@lang('frontend.finish')</th>
-                            <th>@lang('frontend.published')</th>
-                            <th>@lang('frontend.actions')</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($events as $event)
-                            @if(($event->status!='Draft' && (!Sentinel::check())) || Sentinel::check())
-                                <tr>
-                                    <td>{{ $event->title }}</td>
-                                    <td>
-                                        @if ($event->type === 1)
-                                            Online
-                                        @elseif ($event->type === 2)
-                                            Offline
-                                        @elseif ($event->type === 3)
-                                            Online & Offline
-                                        @endif
-                                    </td>
-                                    <td>{{ $event->location }}</td>
-                                    <td>{{ $event->timezone }}</td>
-                                    <td>{{ $event->startt }}</td>
-                                    <td>{{$event->finisht }}</td>
-                                    <td>
-                                        @if ($event->status == 'Publish')
-                                            +
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('events.show', $event->readable_url) }}">
-                                            <i class="fa fa-info-circle" data-name="info" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="view event"></i>
-                                        </a>
-                                        <a href="{{ route('events.edit', $event->readable_url) }}">
-                                            <i class="fa fa-pencil-square-o" data-name="edit" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="edit event"></i>
-                                        </a>
-                                        <a href="{{ route('events.clone', $event->readable_url) }}">
-                                            <i class="fa fa-clone" data-name="clone" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="clone event"></i>
-                                        </a>
-                                        <a href="{{ route('events.confirm-delete', $event->uuid) }}" data-toggle="modal" data-target="#delete_confirm">
-                                            <i class="fa fa-trash-o" data-name="remove-alt" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title="delete event"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endif
-                        @endforeach
+                    {{--<table class="table table-bordered " id="table">--}}
+                        {{--<thead>--}}
+                        {{--<tr class="filters">--}}
+                            {{--<th>@lang('frontend.title')</th>--}}
+                            {{--<th>@lang('frontend.type')</th>--}}
+                            {{--<th>@lang('frontend.location')</th>--}}
+                            {{--<th>Event Time Zone</th>--}}
+                            {{--<th>@lang('frontend.start')</th>--}}
+                            {{--<th>@lang('frontend.finish')</th>--}}
+                            {{--<th>@lang('frontend.published')</th>--}}
+                            {{--<th>@lang('frontend.actions')</th>--}}
+                        {{--</tr>--}}
+                        {{--</thead>--}}
+                        {{--<tbody>--}}
+                        {{--@foreach ($events as $event)--}}
+                            {{--@if(($event->status!='Draft' && (!Sentinel::check())) || Sentinel::check())--}}
+                                {{--<tr>--}}
+                                    {{--<td>{{ $event->title }}</td>--}}
+                                    {{--<td>--}}
+                                        {{--@if ($event->type === 1)--}}
+                                            {{--Online--}}
+                                        {{--@elseif ($event->type === 2)--}}
+                                            {{--Offline--}}
+                                        {{--@elseif ($event->type === 3)--}}
+                                            {{--Online & Offline--}}
+                                        {{--@endif--}}
+                                    {{--</td>--}}
+                                    {{--<td>{{ $event->location }}</td>--}}
+                                    {{--<td>{{ $event->timezone }}</td>--}}
+                                    {{--<td>{{ $event->startt }}</td>--}}
+                                    {{--<td>{{$event->finisht }}</td>--}}
+                                    {{--<td>--}}
+                                        {{--@if ($event->status == 'Publish')--}}
+                                            {{--+--}}
+                                        {{--@else--}}
+                                            {{-----}}
+                                        {{--@endif--}}
+                                    {{--</td>--}}
+                                    {{--<td>--}}
+                                        {{--<a href="{{ route('events.show', $event->readable_url) }}">--}}
+                                            {{--<i class="fa fa-info-circle" data-name="info" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="view event"></i>--}}
+                                        {{--</a>--}}
+                                        {{--<a href="{{ route('events.edit', $event->readable_url) }}">--}}
+                                            {{--<i class="fa fa-pencil-square-o" data-name="edit" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="edit event"></i>--}}
+                                        {{--</a>--}}
+                                        {{--<a href="{{ route('events.clone', $event->readable_url) }}">--}}
+                                            {{--<i class="fa fa-clone" data-name="clone" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="clone event"></i>--}}
+                                        {{--</a>--}}
+                                        {{--<a href="{{ route('events.confirm-delete', $event->uuid) }}" data-toggle="modal" data-target="#delete_confirm">--}}
+                                            {{--<i class="fa fa-trash-o" data-name="remove-alt" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title="delete event"></i>--}}
+                                        {{--</a>--}}
+                                    {{--</td>--}}
+                                {{--</tr>--}}
+                            {{--@endif--}}
+                        {{--@endforeach--}}
 
-                        </tbody>
-                    </table>
+                        {{--</tbody>--}}
+                    {{--</table>--}}
                 </div>
                 <!-- /.tab-pane -->
                 <div class="tab-pane" id="tab_2">
