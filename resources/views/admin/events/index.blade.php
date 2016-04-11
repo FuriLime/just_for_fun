772947e7533f347307e344cd0a918774cd8b7614
X <mykola.d.ysbm@gmail.com>
2016-04-11 10:49:08 +0300

    social buttons

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -99,16 +99,18 @@ events List
                         <div class="col-md-4">
                             <div class="checkbox"><label> <input type="checkbox"> Include past events.</label></div>
                         </div>
-                        <nav class="col-md-4">
-                            <ul class="pagination pagination-sm">
-                                <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">Previous</span></a></li>
-                                <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-                                <li class=""><a href="#">2 <span class="sr-only">(current)</span></a></li>
-                                <li class=""><a href="#">3 <span class="sr-only">(current)</span></a></li>
-                                <li class=""><a href="#">4 <span class="sr-only">(current)</span></a></li>
-                                <li><a href="#" aria-label="Next"><span aria-hidden="true">Next</span></a></li>
-                            </ul>
-                        </nav>
+                        <div class="col-md-4 pagination-holder">
+                            <nav>
+                                <ul class="pagination pagination-sm">
+                                    <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">Previous</span></a></li>
+                                    <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+                                    <li class=""><a href="#">2 <span class="sr-only">(current)</span></a></li>
+                                    <li class=""><a href="#">3 <span class="sr-only">(current)</span></a></li>
+                                    <li class=""><a href="#">4 <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="#" aria-label="Next"><span aria-hidden="true">Next</span></a></li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                     <a href="#" class="btn btn-primary btn-lg"><i class="fa fa-plus"></i> Add New Event</a>
                     {{--<table class="table table-bordered " id="table">--}}
