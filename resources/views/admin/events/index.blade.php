25735f339174d2739913ab6b36340772dc0a6bd8
X <mykola.d.ysbm@gmail.com>
2016-04-11 09:42:56 +0300

    social buttons

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -73,7 +73,7 @@ events List
                                                     <span class="sr-only">Toggle Dropdown</span>
                                                 </button>
                                                 <ul class="dropdown-menu">
-                                                    <li><a href="#">Action</a></li>
+                                                    <li><a href="#"><i class="fa fa-share-alt"></i> Share Event</a></li>
                                                     <li><a href="#">Another action</a></li>
                                                     <li><a href="#">Something else here</a></li>
                                                     <li><a href="#">Separated link</a></li>
