2f39c92ee29e459256dd37a7eeb1651009a78341
X <mykola.d.ysbm@gmail.com>
2016-04-12 16:25:55 +0300

    social buttons

--- resources/views/admin/users/change_pass.blade.php
+++ resources/views/admin/users/change_pass.blade.php
@@ -16,22 +16,9 @@
 
 {{-- Page content --}}
 @section('content')
-    {{--<section class="content-header">--}}
-    {{--<h1>Edit user</h1>--}}
-    {{--<ol class="breadcrumb">--}}
-    {{--<li>--}}
-    {{--<a href="{{ route('dashboard') }}">--}}
-    {{--<i class="livicon" data-name="home" data-size="14" data-color="#000"></i>--}}
-    {{--Dashboard--}}
-    {{--</a>--}}
-    {{--</li>--}}
-    {{--<li>Users</li>--}}
-    {{--<li class="active">Add New User</li>--}}
-    {{--</ol>--}}
-    {{--</section>--}}
     <section class="content">
         <div class="panel-heading clearfix">
-            <h1><i class="fa fa-edit"></i> Edit Profile <a href="#" class="pull-right close-profile" data-toggle="tooltip" data-placement="left" title="Cancel without saving"><i class="fa fa-times-circle-o"></i></a></h1>
+            <h1><i class="fa fa-lock"></i> Change Password <a href="#" class="pull-right close-profile" data-toggle="tooltip" data-placement="left" title="Cancel without saving"><i class="fa fa-times-circle-o"></i></a></h1>
             <div class="user-profile-sub-heading">Please update <strong>Your Profile</strong> so we can add the relevant information to you events. You can set the visibility of your details in your <a href="#">Profile Settings</a></div>
         </div>
         <br />
@@ -50,18 +37,16 @@
                 <!-- CSRF Token -->
                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                 <div class="form-group">
-                <p class="text-warning">If you don't want to change password... please leave them empty</p>
-                <label for="password" class="col-sm-2 control-label">Password *</label>
-                <div class="col-sm-10">
-                <input id="password" name="password" type="password" placeholder="Password" class="form-control" value="{!! Input::old('password') !!}" />
+                    <label for="password" class="col-sm-2 control-label">Password *</label>
+                    <div class="col-sm-10">
+                        <input id="password" name="password" type="password" placeholder="Password" class="form-control" value="{!! Input::old('password') !!}" />
+                    </div>
                 </div>
-                </div>
-
                 <div class="form-group">
-                <label for="password_confirm" class="col-sm-2 control-label">Confirm Password *</label>
-                <div class="col-sm-10">
-                <input id="password_confirm" name="password_confirm" type="password" placeholder="Confirm Password " class="form-control" value="{!! Input::old('password_confirm') !!}" />
-                </div>
+                    <label for="password_confirm" class="col-sm-2 control-label">Confirm Password *</label>
+                    <div class="col-sm-10">
+                        <input id="password_confirm" name="password_confirm" type="password" placeholder="Confirm Password " class="form-control" value="{!! Input::old('password_confirm') !!}" />
+                    </div>
                 </div>
             </form>
         </div>
