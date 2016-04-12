d80517a8d73988a16339d21d8d1cd2a78d7bf6c3
X <mykola.d.ysbm@gmail.com>
2016-04-12 11:17:18 +0300

    social buttons

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,4 +1,4 @@
-{{--@extends('layouts/default')--}}
+@extends('layouts/test')
 
 {{-- Page title --}}
 @section('title')
---
+++ resources/views/layouts/test.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {!! SEO::generate() !!}
+            <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <!--global css starts-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/frontend/custom.css') }}">
+    <!--end of global css-->
+    <!--page level css-->
+    @yield('header_styles')
+            <!--end of page level css-->
+</head>
+
+<body>
+@yield('content')
+</body>
+</html>
