72cbfc8b1142f32451ee27f467c899771c3882ae
X <mykola.d.ysbm@gmail.com>
2016-04-12 11:53:04 +0300

    social buttons

--- resources/views/layouts/without_menu.blade.php
+++ resources/views/layouts/without_menu.blade.php
@@ -9,8 +9,8 @@
             <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <!--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>-->
+    <!--<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>-->
     <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
     <![endif]-->
     <!--global css starts-->
@@ -20,7 +20,7 @@
     <!--end of global css-->
     <!--page level css-->
     @yield('header_styles')
-            <!--end of page level css-->
+    <!--end of page level css-->
 </head>
 
 <body>
