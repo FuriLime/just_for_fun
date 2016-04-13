eea37e8117154418ad7d60cd9d376b60291bd8a3
X <mykola.d.ysbm@gmail.com>
2016-04-13 11:54:45 +0300

    social buttons

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -23,7 +23,9 @@ Edit User
 <div class="admin-edit-profile-holder">
     <div class="panel-heading col-sm-10 col-sm-offset-2 clearfix">
         <h1><i class="fa fa-edit"></i> Edit Profile <a href="#" class="pull-right close-profile" data-toggle="tooltip" data-placement="left" title="Cancel without saving"><i class="fa fa-times-circle-o"></i></a></h1>
-        <div class="user-profile-sub-heading">Please update <strong>Your Profile</strong> so we can add the relevant information to you events. You can set the visibility of your details in your <a href="#">Profile Settings</a></div>
+        <div class="row">
+            <div class="user-profile-sub-heading">Please update <strong>Your Profile</strong> so we can add the relevant information to you events. You can set the visibility of your details in your <a href="#">Profile Settings</a></div>
+        </div>
     </div>
     <br />
     <div class="panel">
@@ -39,8 +41,8 @@ Edit User
         </div>
         <form class="form-horizontal edit-profile-form" action="" method="POST" enctype="multipart/form-data">
             <div class="form-group">
-                <label class="col-sm-2 control-label">Profile Image</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Profile Image</label>
+                <div class="col-sm-6">
                     <div class="fileinput fileinput-new" data-provides="fileinput">
                         <div class="fileinput-new thumbnail" style="width: 200px; height: 200px;">
                             @if($user_profile->image)
@@ -63,53 +65,53 @@ Edit User
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Username</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Username</label>
+                <div class="col-sm-6">
                     <input type="text" class="form-control">
                     <i class="fa fa-info-circle infopoint" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Some content in Popover on right" data-original-title="Popover title"></i>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">First Name</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">First Name</label>
+                <div class="col-sm-6">
                     <input type="text" name="first_name" class="form-control required" value="{!! Input::old('first_name', $user->first_name) !!}">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Last Name</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Last Name</label>
+                <div class="col-sm-6">
                     <input type="text" name="last_name" class="form-control required" value="{!! Input::old('last_name', $user->last_name) !!}" >
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Email Address</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Email Address</label>
+                <div class="col-sm-6">
                     <input type="text" name="email" class="form-control required email" value="{!! Input::old('email', $user->email) !!}">
                     <i class="fa fa-info-circle infopoint" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Some content in Popover on right" data-original-title="Popover title"></i>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">About you</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">About you</label>
+                <div class="col-sm-6">
                     <textarea class="form-control" name="bio">{!! Input::old('bio', $user_profile->bio) !!}</textarea>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Country</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Country</label>
+                <div class="col-sm-6">
                     {!! Form::select('country', $countries,Input::old('country',$user_profile->country),array('class' => 'form-control')) !!}
                     <i class="fa fa-info-circle infopoint" title="" data-container="body" data-toggle="popover" data-placement="right" data-content="Some content in Popover on right" data-original-title="Popover title"></i>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Job Title</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Job Title</label>
+                <div class="col-sm-6">
                     <input type="text" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Gender</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Gender</label>
+                <div class="col-sm-6">
                     <label class="radio-inline">
                         <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male" @if($user->gender === 'male') selected="selected" @endif > Male
                     </label>
@@ -122,37 +124,37 @@ Edit User
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Date of Birth</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Date of Birth</label>
+                <div class="col-sm-6">
                     <input type="text" name="dob" class="form-control date-of-birth" data-mask="9999-99-99" value="{!! Input::old('dob', $user_profile->dob) !!}" placeholder="yyyy-mm-dd">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Linkedin URL</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Linkedin URL</label>
+                <div class="col-sm-6">
                     <input type="text" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Twitter Username</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Twitter Username</label>
+                <div class="col-sm-6">
                     <input type="text" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Facebook Username</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Facebook Username</label>
+                <div class="col-sm-6">
                     <input type="text" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-sm-2 control-label">Google+ URL</label>
-                <div class="col-sm-8">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Google+ URL</label>
+                <div class="col-sm-6">
                     <input type="text" class="form-control">
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-8">
+                <div class="col-sm-offset-4 col-sm-6">
                     <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Update Profile</button>
                     <button class="btn btn-default">Cancel</button>
                 </div>
