64ceb8d8870195e66309dd39bf16bb117943a019
X <mykola.d.ysbm@gmail.com>
2016-04-13 11:31:22 +0300

    social buttons

--- resources/views/admin/layouts/without_menu.blade.php
+++ resources/views/admin/layouts/without_menu.blade.php
@@ -21,10 +21,6 @@
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- font Awesome -->
     <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('assets/css/styles/black.css') }}" rel="stylesheet" type="text/css" id="colorscheme" />
-    <link href="{{ asset('assets/css/panel.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('assets/css/metisMenu.css') }}" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="{{ asset('assets/css/only_dashboard.css') }}" />
 
     <!-- end of global css -->
     <!--page level css-->
@@ -36,21 +32,9 @@
     <div class="container">
         @yield('content')
     </div>
-    <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Return to top" data-toggle="tooltip" data-placement="left">
-        <i class="livicon" data-name="plane-up" data-size="18" data-loop="true" data-c="#fff" data-hc="white"></i>
-    </a>
     <!-- global js -->
     <script src="{{ asset('assets/js/jquery-1.11.1.min.js') }}" type="text/javascript"></script>
-    @if (Request::is('admin/form_builder2') || Request::is('admin/gridmanager') || Request::is('admin/portlet_draggable') || Request::is('admin/calendar'))
-        <script src="{{ asset('assets/vendors/form_builder1/js/jquery.ui.min.js') }}"></script>
-    @endif
     <script src="{{ asset('assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
-    <!--livicons-->
-    <script src="{{ asset('assets/js/josh.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/js/metisMenu.js') }}" type="text/javascript"> </script>
-    <script src="{{ asset('assets/vendors/holder-master/holder.js') }}" type="text/javascript"></script>
-    <!-- end of global js -->
-    <!-- begin page level js -->
     @yield('footer_scripts')
     <!-- end page level js -->
 </body>
