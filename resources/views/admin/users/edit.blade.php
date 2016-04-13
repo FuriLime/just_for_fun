63680321a47a4a7c2202e6389e099d8b86a80cce
X <mykola.d.ysbm@gmail.com>
2016-04-13 12:11:38 +0300

    social buttons

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -22,6 +22,61 @@ Edit User
 @section('content')
 <div class="admin-edit-profile-holder">
     <div class="panel-heading col-sm-10 col-sm-offset-2 clearfix">
+        <h1><i class="fa fa-edit"></i> Edit Profile Settings <a href="#" class="pull-right close-profile" data-toggle="tooltip" data-placement="left" title="Cancel without saving"><i class="fa fa-times-circle-o"></i></a></h1>
+        <div class="row">
+            <div class="user-profile-sub-heading col-sm-10">Here you can manage the settings for your Profile. You can update your Profile details under <a href="#">Profile</a></div>
+        </div>
+    </div>
+    <br />
+    <div class="panel">
+        <!-- errors -->
+        <div class="has-error">
+            {!! $errors->first('first_name', '<span class="help-block">:message</span>') !!}
+            {!! $errors->first('last_name', '<span class="help-block">:message</span>') !!}
+            {!! $errors->first('email', '<span class="help-block">:message</span>') !!}
+            {!! $errors->first('password', '<span class="help-block">:message</span>') !!}
+            {!! $errors->first('password_confirm', '<span class="help-block">:message</span>') !!}
+            {!! $errors->first('group', '<span class="help-block">:message</span>') !!}
+            {!! $errors->first('image', '<span class="help-block">:message</span>') !!}
+        </div>
+        <form class="form-horizontal edit-profile-form" action="" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Timezone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Date Format</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Time Format</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 col-sm-offset-2 control-label">Visibility Profile Page</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-6">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Update Profile Settings</button>
+                    <button class="btn btn-default">Cancel</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<div class="admin-edit-profile-holder">
+    <div class="panel-heading col-sm-10 col-sm-offset-2 clearfix">
         <h1><i class="fa fa-edit"></i> Edit Profile <a href="#" class="pull-right close-profile" data-toggle="tooltip" data-placement="left" title="Cancel without saving"><i class="fa fa-times-circle-o"></i></a></h1>
         <div class="row">
             <div class="user-profile-sub-heading col-sm-10">Please update <strong>Your Profile</strong> so we can add the relevant information to you events. You can set the visibility of your details in your <a href="#">Profile Settings</a></div>
